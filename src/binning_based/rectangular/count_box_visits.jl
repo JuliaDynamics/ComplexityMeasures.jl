300714b4970c7362a0a93c43c15fcea1ccd63432
Kristian Agasøster Haaga <kahaaga@gmail.com>
2020-10-26 09:09:27 +0100

    Move encode_as_bin

--- src/binning_based/rectangular/count_box_visits.jl
+++ src/binning_based/rectangular/count_box_visits.jl
@@ -1,8 +1,51 @@
+import DelayEmbeddings: AbstractDataset, minima, maxima
+import StaticArrays: SVector, MVector
 
-export joint_visits, marginal_visits
+export joint_visits, marginal_visits, encode_as_bin 
 
-# Count bin visits by counting equal encoded bins. Either for all dimensions at a 
-# time (`joint_visits`), or for a subset of dimensions (`marginal_visits`).
+"""
+    encode_as_bin(point, reference_point, edgelengths) → Vector{Int}
+
+Encode a point into its integer bin labels relative to some `reference_point`
+(always counting from lowest to highest magnitudes), given a set of box 
+`edgelengths` (one for each axis). The first bin on the positive side of 
+the reference point is indexed with 0, and the first bin on the negative 
+side of the reference point is indexed with -1.
+
+See also: [`joint_visits`](@ref), [`marginal_visits`](@ref).
+
+## Example 
+
+```julia
+using Entropies
+
+refpoint = [0, 0, 0]
+steps = [0.2, 0.2, 0.3]
+encode_as_bin(rand(3), refpoint, steps)
+```
+"""
+function encode_as_bin end 
+
+function encode_as_bin(point::Vector{T}, reference_point, edgelengths) where {T <: Real}
+    floor.(Int, (point .- reference_point) ./ edgelengths)
+end
+
+function encode_as_bin(point::SVector{dim, T}, reference_point, edgelengths) where {dim, T <: Real}
+    floor.(Int, (point .- reference_point) ./ edgelengths)
+end
+
+function encode_as_bin(point::MVector{dim, T}, reference_point, edgelengths) where {dim, T <: Real}
+    floor.(Int, (point .- reference_point) ./ edgelengths)
+end
+
+function encode_as_bin(points::Vector{T}, reference_point, edgelengths) where {T <: Union{Vector, SVector, MVector}}
+    [encode_as_bin(points[i], reference_point, edgelengths) for i = 1:length(points)]
+end
+
+function encode_as_bin(points::AbstractDataset{dim, T}, reference_point, edgelengths) where {dim, T}
+    [encode_as_bin(points[i], reference_point, edgelengths) for i = 1:length(points)]
+end
+
 
 """
     joint_visits(points, binning_scheme::RectangularBinning) → Vector{Vector{Int}}
--- src/binning_based/rectangular/rectangular_estimators.jl
+++ src/binning_based/rectangular/rectangular_estimators.jl
@@ -2,9 +2,7 @@ abstract type BinningProbabilitiesEstimator <: ProbabilitiesEstimator end
 
 include("binning_schemes.jl")
 
-include("utils.jl")
 include("count_box_visits.jl")
 include("histogram_estimation.jl")
 
-# Estimators
 include("VisitationFrequency.jl")
