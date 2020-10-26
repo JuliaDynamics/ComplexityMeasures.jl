d9a8e1493d72cce3384c918aeaf562fbc6bbd1e8
Kristian Agasøster Haaga <kahaaga@gmail.com>
2020-10-26 08:54:11 +0100

    rename file

--- src/symbolic/symbolic.jl
+++ src/symbolic/symbolic.jl
@@ -6,7 +6,7 @@ A probability estimator based on symbolization.
 abstract type SymbolicProbabilityEstimator <: ProbabilitiesEstimator end
 
 include("utils.jl")
-include("symbolic_permutation.jl")
+include("SymbolicPermutation.jl")
 
 
 
