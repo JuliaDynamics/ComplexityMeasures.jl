c040e9311b64e3bff8eadeb2649ba2a75e27c913
Datseris <datseris.george@gmail.com>
2020-11-09 14:02:41 +0100

    add Probabilities type

--- src/abstract.jl
+++ src/abstract.jl
@@ -1,9 +1,22 @@
-export ProbabilitiesEstimator, entropy, entropy!, genentropy
+export ProbabilitiesEstimator, entropy, entropy!, genentropy, Probabilities
+
+struct Probabilities{T}
+    p::Vector{T}
+    function new(x)
+        T = eltype(x)
+        s = sum(x)
+        if s ≠ 1
+            x = x ./ s
+        end
+        return Probabilities{T}(x)
+    end
+end
+
 
 """
 An abstract type for probabilities estimators.
 """
-abstract type ProbabilitiesEstimator end 
+abstract type ProbabilitiesEstimator end
 
 
 function entropy end
