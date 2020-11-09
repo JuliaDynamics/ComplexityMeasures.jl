efee0dc0c687f2024a6070dc8ff11c197fd4e4f3
Datseris <datseris.george@gmail.com>
2020-11-09 14:15:39 +0100

    re-write the call signature of `non0hist`

--- src/histogram_estimation.jl
+++ src/histogram_estimation.jl
@@ -2,44 +2,39 @@ export non0hist, binhist
 
 import DelayEmbeddings: AbstractDataset
 
-""" 
-# Histograms from collections
+"""
+    non0hist(x::Vector_or_Dataset) → p::Probabilities
 
-    non0hist(x::AbstractVector; normalize::Bool = true) → p::Vector{Float64}
-
-Compute the unordered histogram of the values of `x`, directly from the distribution of 
+Compute the unordered histogram of the values of `x`, directly from the distribution of
 values, without any coarse-graining or discretization. Assumes that `x` can be sorted.
 
-If `normalize==true`, then the 
-histogram is sum-normalized. If `normalize==false`, then occurrence counts for the unique 
-elements in `x` is returned. 
+    non0hist(dataset::Vector_or_Dataset, ε::Real) → p::Probabilities
 
-## Example 
+Partition a dataset into tabulated intervals (boxes) of
+size `ε` and return the sum-normalized histogram in an
+unordered 1D form, discarding all zero elements and bin edge information.
+Use [`binhist`](@ref) to retain bin edge information.
 
-```julia
-using Entropies
-x = rand(1:10, 100000)
-Entropies.non0hist(x) # sum-normalized
-Entropies.non0hist(x, normalize = false) # histogram (counts)
-```
+This method has a linearithmic time complexity (`n log(n)` for `n = length(data)`)
+and a linear space complexity (`l` for `l = dimension(data)`).
+This allows computation of histograms of high-dimensional
+datasets and with small box sizes `ε` without memory overflow and with maximum performance.
 
-# Histograms of `Dataset`s
+Notice that `non0hist` returns probabilities. To obtain counts simply do
+`counts = p * length(x)`.
 
-    non0hist(x::AbstractDataset; normalize::Bool = true) → p::Vector{Float64}
-
-Compute the unordered histogram of the values of the `Dataset` `x` , directly from the 
-distribution of points, without any coarse-graining or discretization.
-
-## Example 
+## Example
 
 ```julia
 using DelayEmbeddings, Entropies
-D = Dataset(rand(1:3, 50000, 3))
-Entropies.non0hist(D) # sum-normalized
-Entropies.non0hist(D, normalize = false) # histogram (counts)
+A = Dataset(rand(1:3, 50000, 3))
+non0hist(A)
+# discretizing version
+B = Dataset(rand(1000, 3))
+non0hist(B, 0.01)
 ```
 """
-function non0hist(x::AbstractVector{T}; normalize::Bool = true) where T
+function non0hist(x::AbstractVector{T}) where T
     L = length(x)
 
     hist = Vector{Float64}()
@@ -63,34 +58,12 @@ function non0hist(x::AbstractVector{T}; normalize::Bool = true) where T
 
     # Shrink histogram capacity to fit its size:
     sizehint!(hist, length(hist))
-    if normalize 
-        return hist ./ L
-    else 
-        return hist
-    end
+    return Probabilities(hist ./ L)
 end
 
-# The following is from chaostools
-##################################
-"""
-    non0hist(ε, dataset::AbstractDataset; normalize = true) → p
-
-Partition a dataset into tabulated intervals (boxes) of
-size `ε` and return the (sum-normalized, if `normalize==true`) histogram in an 
-unordered 1D form, discarding all zero elements and bin edge information.
-
-## Performances Notes
-This method has a linearithmic time complexity (`n log(n)` for `n = length(data)`)
-and a linear space complexity (`l` for `l = dimension(data)`).
-This allows computation of histograms of high-dimensional
-datasets and with small box sizes `ε` without memory overflow and with maximum performance.
-
-Use [`binhist`](@ref) to retain bin edge information.
-"""
-non0hist(args...) = _non0hist(args...)[1]
-
-
-function _non0hist(ε::Real, data::AbstractDataset{D, T}; normalize::Bool = true) where {D, T<:Real}
+# The following is originally from ChaosTools.jl
+non0hist(data::AbstractDataset, ε::Real) = _non0hist(data, ε)[1]
+function _non0hist(data::AbstractDataset{D, T}, ε::Real) where {D, T<:Real}
     mini = minima(data)
     L = length(data)
     hist = Vector{Float64}()
@@ -116,20 +89,15 @@ function _non0hist(ε::Real, data::AbstractDataset{D, T}; normalize::Bool = true
 
     # Shrink histogram capacity to fit its size:
     sizehint!(hist, length(hist))
-
-    if normalize 
-        return hist ./ L, bins, mini
-    else 
-        return hist, bins, mini
-    end
+    return Propabilities(hist ./ L), bins, mini
 end
 
 """
-    binhist(ε, data; normalize = true) → p, bins
+    binhist(data::Dataset, ε::Real; normalize = true) → p, bins
 Do the same as [`non0hist`](@ref) but also return the bin edge information.
 """
-function binhist(ε, data; normalize::Bool = true)
-    hist, bins, mini = _non0hist(ε, data, normalize = normalize)
+function binhist(data, ε)
+    hist, bins, mini = _non0hist(ε, data)
     unique!(bins)
     b = [β .* ε .+ mini for β in bins]
     return hist, b
