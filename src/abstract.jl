4f887cc3ff375590f95a1eff848b9e2ab3d6b8e7
Datseris <datseris.george@gmail.com>
2020-11-09 14:23:54 +0100

    exrtend Vector interface for Probabilities

--- src/abstract.jl
+++ src/abstract.jl
@@ -2,16 +2,25 @@ export ProbabilitiesEstimator, entropy, entropy!, genentropy, Probabilities
 
 struct Probabilities{T}
     p::Vector{T}
-    function new(x)
+    function Probabilities(x)
         T = eltype(x)
         s = sum(x)
         if s ≠ 1
             x = x ./ s
         end
-        return Probabilities{T}(x)
+        return new{T}(x)
     end
 end
 
+
+# extend base Vector interface:
+for f in (:length, :size, :IteratorSize, :eachindex, :eltype, :lastindex, :firstindex)
+    @eval Base.$(f)(d::Probabilities) = $(f)(d.p)
+end
+@inline Base.iterate(d::Probabilities, i = 1) = iterate(d.p, i)
+@inline Base.getindex(d::Probabilities, i) = d.p[i]
+@inline Base.:*(d::Probabilities, x::Number) = d.p * x
+@inline Base.sum(d::Probabilities{T}) = one(T)
 
 """
 An abstract type for probabilities estimators.
