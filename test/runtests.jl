6e8966a6009909e1c8fbbb187339b28d668802ad
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-02-20 15:16:58 +0100

    Temporarily reduce version to fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,14 @@
-include("install_dependencies.jl")
+if VERSION >= v"0.7.0-"
+    # Adding Pkg in test/REQUIRE would be an error in 0.6.  Using
+    # Project.toml still has some gotchas.  So:
+    Pkg = Base.require(Base.PkgId(Base.UUID(0x44cfe95a1eb252eab672e2afdf69b78f), "Pkg"))
+end
+
+ENV["PYTHON"] = ""
+Pkg.build("PyCall")
+
+using Conda
+Conda.add("scipy")
 
 using Test
 using Entropies
