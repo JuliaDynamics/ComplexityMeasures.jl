b531961e0c49b9b65f172d1871867e49d0ea863a
Datseris <datseris.george@gmail.com>
2022-09-04 10:25:28 +0100

    Working code both periodic and not

--- src/symbolic/symbolic_stencils.jl
+++ src/symbolic/symbolic_stencils.jl
@@ -1,19 +1,23 @@
 """
-    SpatiotemporalPermutation(stencil, periodic = true)
-A symbolic, permutation-based probabilities/entropy estimator for higher-dimensional
-data, typically representing spatiotemporal systems. The data are higher-dimensional
-arrays, such as 2D [^Ribeiro2012] or 3D [^Schlemmer2018].
+    SpatiotemporalPermutation(stencil, x, periodic = true)
+A symbolic, permutation-based probabilities/entropy estimator for spatiotemporal systems.
+The data are a high-dimensional array `x`, such as 2D [^Ribeiro2012] or 3D [^Schlemmer2018].
 This approach is also known as _Spatiotemporal Permutation Entropy_.
+`x` is given because we need to know its size for optimization and bound checking.
 
 A _stencil_ defines what local area around each pixel to
 consider, and compute the ordinal pattern within the stencil. Stencils are given as
 vectors of `CartesianIndex` which encode the _offsets_ of the pixes to include in the
-stencil, with respect to the current pixel. For example
+stencil, with respect to the current pixel. For simplicity, only zero or positive offsets
+are allowed. For example
 ```julia
+data = [rand(50, 50) for _ in 1:50]
+x = data[1] # first "time slice" of a spatial system evolution
 stencil = CartesianIndex.([(0,1), (0,1), (1,1)])
-est = SpatiotemporalPermutation(stencil)
+est = SpatiotemporalPermutation(stencil, x)
 ```
-Here the stencil creates a 2x2 square extending to the bottom and right of the pixel.
+Here the stencil creates a 2x2 square extending to the bottom and right of the pixel
+(directions here correspond to the way Julia prints matrices by default).
 Notice that no offset (meaning the pixel itself) is always included automatically.
 The length of the stencil decides the order of the permutation entropy, and the ordering
 within the stencil dictates the order that pixels are compared with.
@@ -23,8 +27,8 @@ After having defined `est`, one calculates the permutation entropy of ordinal pa
 by calling [`genentropy`](@ref) with `est`, and with the array data.
 To apply this to timeseries of spatial data, simply loop over the call, e.g.:
 ```julia
-data = [rand(50, 50) for _ in 1:50]
-entropies = [genentropy(x, est) for x in data]
+entropy = genentropy(x, est)
+entropy_vs_time = genentropy.(data, est) # broadcasting with `.`
 ```
 
 The argument `periodic` decides whether the stencil should wrap around at the end of the
@@ -38,50 +42,58 @@ array. If `periodic = false`, pixels whose stencil exceeds the array bounds are 
     Schlemmer et al. (2012). Spatiotemporal Permutation Entropy as a Measure for
     Complexity of Cardiac Arrhythmia. https://doi.org/10.3389/fphy.2018.00039
 """
-struct SpatiotemporalPermutation{D,P} <: ProbabilitiesEstimator
+struct SpatiotemporalPermutation{D,P,V} <: ProbabilitiesEstimator
     stencil::Vector{CartesianIndex{D}}
     viewer::Vector{CartesianIndex{D}}
+    arraysize::Dims{D}
+    valid::V
 end
 function SpatiotemporalPermutation(
-        stencil::Vector{CartesianIndex{D}}, p::Bool = true
+        stencil::Vector{CartesianIndex{D}}, x::AbstractArray, p::Bool = true
     ) where {D}
     # Ensure that no offset is part of the stencil
     stencil = pushfirst!(copy(stencil), CartesianIndex{D}(zeros(Int, D)...))
-    SpatiotemporalPermutation{D, p}(stencil, copy(stencil))
+    arraysize = size(x)
+    # Store valid indices for later iteration
+    if p
+        valid = CartesianIndices(x)
+    else
+        # collect maximum offsets in each dimension for limiting ranges
+        maxoffsets = [maximum(s[i] for s in stencil) for i in 1:D]
+        ranges = Iterators.product([1:(arraysize[i]-maxoffsets[i]) for i in 1:D]...)
+        valid = Base.Generator(idxs -> CartesianIndex{D}(idxs), ranges)
+    end
+    SpatiotemporalPermutation{D, p, typeof(valid)}(stencil, copy(stencil), arraysize, valid)
 end
 
 # This source code is a modification of the code of Agents.jl that finds neighbors
 # in grid-like spaces. It's the code of `nearby_positions` in `grid_general.jl`.
-function pixels_in_stencil(array, pixel, spatperm::SpatiotemporalPermutation{D,false}) where {D}
-    # TODO: Not clear yet how out of bounds is handled in this case.
+function pixels_in_stencil(pixel, spatperm::SpatiotemporalPermutation{D,false}) where {D}
     @inbounds for i in eachindex(spatperm.stencil)
-        statperm.viewer[i] = spatperm.stencil[i] + pixel
-    end
-    return statperm.viewer
-    # space_size = size(array)
-    # positions_iterator = (n + pixel for n in spatperm.stencil)
-    # return Base.Iterators.filter(
-    #     pos -> checkbounds(Bool, array, pos...), positions_iterator
-    # )
-end
-
-function pixels_in_stencil(array, pixel, spatperm::SpatiotemporalPermutation{D,true}) where {D}
-    space_size = size(array)
-    @inbounds for i in eachindex(spatperm.stencil)
-        spatperm.viewer[i] = CartesianIndex{D}(mod1.(Tuple(spatperm.stencil[i] + pixel), space_size))
+        spatperm.viewer[i] = spatperm.stencil[i] + pixel
     end
     return spatperm.viewer
 end
 
-function Entropies.probabilities(x, est::SpatiotemporalPermutation) where {m, T}
-    s = zeros(Int, length(x))
+function pixels_in_stencil(pixel, spatperm::SpatiotemporalPermutation{D,true}) where {D}
+    arraysize = spatperm.arraysize
+    @inbounds for i in eachindex(spatperm.stencil)
+        # It's annoying that we have to change to tuple and then to CartesianIndex
+        # because iteration over cartesian indices is not allowed. But oh well.
+        spatperm.viewer[i] = CartesianIndex{D}(mod1.(Tuple(spatperm.stencil[i] + pixel), arraysize))
+    end
+    return spatperm.viewer
+end
+
+function Entropies.probabilities(x, est::SpatiotemporalPermutation)
+    s = zeros(Int, length(est.valid))
     probabilities!(s, x, est)
 end
 
 function Entropies.probabilities!(s::AbstractVector{Int}, x, est::SpatiotemporalPermutation)
     m = length(est.stencil)
-    for (i, pixel) in enumerate(CartesianIndices(x))
-        pixels = pixels_in_stencil(x, pixel, est)
+    for (i, pixel) in enumerate(est.valid)
+        pixels = pixels_in_stencil(pixel, est)
         s[i] = Entropies.encode_motif(view(x, pixels), m)
     end
     return probabilities(s)
@@ -89,6 +101,10 @@ end
 
 # %%
 stencil = CartesianIndex.([(0,1), (0,1), (1,1)])
-est = SpatiotemporalPermutation(stencil)
 x = rand(50, 50)
+est = SpatiotemporalPermutation(stencil, x, false)
 p = probabilities(x, est)
+
+using BenchmarkTools
+s = zeros(Int, length(est.valid))
+@btime probabilities!($s, $x, $est)
