579c716b3c9a215b96f90cb2ed98a800e4907bdb
Kristian Haaga <kahaaga@gmail.com>
2022-09-27 15:46:58 +0200

    Update version to 2.0.0 and new update message

--- src/Entropies.jl
+++ src/Entropies.jl
@@ -28,17 +28,17 @@ include("deprecations.jl")
 # Update messages:
 using Scratch
 display_update = true
-version_number = "1.3.0"
+version_number = "2.0.0"
 update_name = "update_v$(version_number)"
 update_message = """
 \nUpdate message: Entropies v$(version_number)\n
 - An overall overhaul of the documentation and API of Entropies.jl has been performed.
-  Now `genentropy` is `entropy_renyi`. The documentation further clarifies
-  the difference between calculating probabilities and entropies.
-- A huge amount of new content has been added to Entropies, which is best seen
-  by visiting the changelog or the new documentation.
-  E.g., Tsallis entropy, spatial permutation entropy, dispersion entropy, and many more.
-- New exported API function: `symbolize`.
+- A huge amount of new content has been added, which is best seen by visiting the changelog
+    or the online documentation. Some examples are Tsallis entropy and spatial permutation
+        entropy, and much more.
+- In summary, all entropies and normalized entropies are computing using the
+    `entropy` and `entropy_normalized` functions, which dispatch on entropy types such
+    as `Renyi()`, `Shannon()` or `Tsallis()`.
 - New constructors for symbolizing: `OrdinalPattern, GaussianSymbolization`.
 """
 
