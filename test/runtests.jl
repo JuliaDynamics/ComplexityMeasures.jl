5cf63d9a4d58d7ae68ec52db2fa17943e6b8fe5c
Datseris <datseris.george@gmail.com>
2022-09-28 21:18:46 +0100

    even better automated test setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,13 @@
 using Test
-using Entropies
 # TODO: None of these packages should be used here. Instead, the files they are needed
+using Entropies
 using Entropies.DelayEmbeddings
 using Entropies.DelayEmbeddings.StaticArrays
 
-defaultname(file) = splitext(basename(file))[1]
+defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 @testset "Entropies.jl" begin
-    # Different generalized entropies
-    testfile("entropies/renyi.jl")
-    testfile("entropies/shannon.jl")
-    testfile("entropies/tsallis.jl")
-
-    # Probability and entropy estimators
+    # Probability estimators
     testfile("estimators/counting_based.jl")
     testfile("estimators/timescales.jl")
     testfile("estimators/dispersion.jl")
@@ -23,7 +18,12 @@ testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include
     testfile("estimators/permutation_spatial.jl")
     testfile("estimators/visitation_frequency.jl")
     testfile("estimators/transfer_operator.jl")
-    testfile("estimators/nn.jl", "nearest neighbors direct")
+
+    # Different entropies
+    testfile("entropies/renyi.jl")
+    testfile("entropies/shannon.jl")
+    testfile("entropies/tsallis.jl")
+    testfile("entropies/nearest_neighbors_direct.jl")
 
     # Various
     testfile("complexity_measures/complexity_measures.jl")
