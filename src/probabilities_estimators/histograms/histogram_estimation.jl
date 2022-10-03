51149ac2b857a19f3171119c2ada07d074f2a155
Kristian Haaga <kahaaga@gmail.com>
2022-10-04 00:30:05 +0200

    Fix dispersion

--- src/probabilities_estimators/histograms/histogram_estimation.jl
+++ src/probabilities_estimators/histograms/histogram_estimation.jl
@@ -28,11 +28,11 @@ function fasthist(x::Vector_or_Dataset, ϵ::AbstractBinning)
 end
 
 """
-    fasthist(x) → c::Vector{Int}
+    fasthist(x, eltype::T = Int) where T <: Number → c::Vector{Q}
 
 Count the occurrences `c` of the unique data values in `x`,
 
-Returns a `Vector{Int}`, where `cᵢ ∈ c` is the number of times the value
+Returns a `Vector{T}`, where `cᵢ ∈ c` is the number of times the value
 `sort!(unique(x))[i]` occurs. Hence, this method is useful mostly when
 `x` contains integer or categorical data. The sorted values themselves
 are not returned - only their frequencies.
@@ -41,9 +41,9 @@ Prior to counting, `x` is sorted, so this function also mutates `x`.
 Therefore, it is called with `copy` in higher level API when necessary.
 This function works for any `x` for which `sort!(x)` works.
 """
-function fasthist(x)
+function fasthist(x, eltype::Type{T} = Int) where T <: Number
     L = length(x)
-    hist = Vector{Int}()
+    hist = Vector{T}()
     # Reserve enough space for histogram:
     sizehint!(hist, L)
     # Fill the histogram by counting consecutive equal values:
@@ -51,13 +51,14 @@ function fasthist(x)
     prev_val, count = x[1], 0
     for val in x
         if val == prev_val
-            count += 1
+            count += one(eltype)
         else
             push!(hist, count)
             prev_val = val
-            count = 1
+            count = one(eltype)
         end
     end
+
     push!(hist, count)
     # Shrink histogram capacity to fit its size:
     sizehint!(hist, length(hist))
