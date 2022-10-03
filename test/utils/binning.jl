3d14d9c380333d5e29af26879c763e8bb8901659
Kristian Haaga <kahaaga@gmail.com>
2022-10-04 00:30:17 +0200

    Test `fasthist` properly

--- test/utils/binning.jl
+++ test/utils/binning.jl
@@ -1,14 +1,14 @@
 @testset "Histogram estimation" begin
     x = rand(1:10, 100)
     D = Dataset([rand(1:10, 3) for i = 1:100])
-    D2 = [(rand(1:10), rand(1:10, rand(1:10)) for i = 1:100)]
-    @test Entropies.fasthist(x) isa Probabilities
-    @test Entropies.fasthist(D) isa Probabilities
-    @test Entropies.fasthist(D2) isa Probabilities
+    D2 = [(rand(1:10), rand(1:10), rand(1:10)) for i = 1:100]
 
-    @test Entropies.fasthist(x) |> sum ≈ 1.0
-    @test Entropies.fasthist(D) |> sum ≈ 1.0
-    @test Entropies.fasthist(D2)|> sum ≈ 1.0
+    @test Entropies.fasthist(x) isa Vector{Int}
+    @test Entropies.fasthist(D) isa Vector{Int}
+    @test Entropies.fasthist(D2) isa Vector{Int}
+    @test sum(Entropies.fasthist(x)) == length(x)
+    @test sum(Entropies.fasthist(D)) == length(D)
+    @test sum(Entropies.fasthist(D2)) == length(D2)
 end
 
 @testset "Shorthand" begin
