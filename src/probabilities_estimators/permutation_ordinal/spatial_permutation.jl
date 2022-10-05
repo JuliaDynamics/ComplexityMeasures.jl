5b1d4fcf9b86f510b86471ce14723a20dbd35532
Inga Kottlarz <69455494+ikottlarz@users.noreply.github.com>
2022-10-05 22:28:05 +0200

    Added convenience methods for passing stencils to functions (#121)
    * added convenience methods for passing stencils to functions
    
    * added new function signature to docstring
    
    * docstring modifications, generalized extent-lag approach to higher dimensions
    
    * removed auto-addition of 0-offset, changed signature of extent/lag version, rewrote docstring, adjusted tests
    
    * included requested changes on docstring
    
    * update ambiguous docstring formulation
    
    * include  in docstring for all methods
    
    Co-authored-by: Inga Kottlarz <inga.kottlarz@stud.uni-goettingen.de>
    Co-authored-by: Datseris <datseris.george@gmail.com>

--- src/probabilities_estimators/permutation_ordinal/spatial_permutation.jl
+++ src/probabilities_estimators/permutation_ordinal/spatial_permutation.jl
@@ -9,24 +9,56 @@ The input data `x` are high-dimensional arrays, for example 2D arrays [^Ribeiro2
 [^Schlemmer2018]. This approach is also known as _spatiotemporal permutation entropy_.
 `x` is given because we need to know its size for optimization and bound checking.
 
-A _stencil_ defines what local area around each pixel to
-consider, and compute the ordinal pattern within the stencil. Stencils are given as
-vectors of `CartesianIndex` which encode the _offsets_ of the pixes to include in the
-stencil, with respect to the current pixel. For example
+A _stencil_ defines what local area (which points) around each pixel to
+consider, and compute ordinal patterns from.
+The number of included points in a stencil (`m`) determines the length of the vectors
+to be symbolized, i.e. there are `m!` possible ordinal patterns around each pixel.
 
+Example usage:
 ```julia
 data = [rand(50, 50) for _ in 1:50]
 x = data[1] # first "time slice" of a spatial system evolution
-stencil = CartesianIndex.([(0,1), (1,1), (1,0)])
+stencil = ...
 est = SpatialSymbolicPermutation(stencil, x)
 ```
 
-Here the stencil creates a 2x2 square extending to the bottom and right of the pixel
-(directions here correspond to the way Julia prints matrices by default).
-Notice that no offset (meaning the pixel itself) is always included automatically.
-The length of the stencil decides the order of the permutation entropy, and the ordering
-within the stencil dictates the order that pixels are compared with.
-The pixel without any offset is always first in the order.
+
+Stencils are passed in one of the following three ways:
+
+1. As vectors of `CartesianIndex` which encode the pixels to include in the
+    stencil, with respect to the current pixel, or integer arrays of the same dimensionality
+    as the data. For example
+
+    ```julia
+    stencil = CartesianIndex.([(0,0), (0,1), (1,1), (1,0)])
+    ```
+    Don't forget to include the zero offset index if you want to include the point itself, 
+    which is almost always the case.
+    Here the stencil creates a 2x2 square extending to the bottom and right of the pixel
+    (directions here correspond to the way Julia prints matrices by default).
+    When passing a stencil as a vector of `CartesianIndex`, `m = length(stencil)`.
+
+2. As a `D`-dimensional array (where `D` matches the dimensionality of the input data)
+    containing `0`s and `1`s, where if `stencil[index] == 1`, the corresponding pixel is
+    included, and if `stencil[index] == 0`, it is not included.
+    To generate the same estimator as in 1., use
+
+    ```julia
+    stencil = [1 1; 1 1]
+    ```
+    When passing a stencil as a `D`-dimensional array, `m = sum(stencil)`
+
+3. As a `Tuple` containing two `Tuple`s, both of length `D`, for `D`-dimensional data.
+    The first tuple specifies the `extent` of the stencil, where `extent[i]` 
+    dictates the number of pixels to be included along the `i`th axis and `lag[i]`
+    the separation of pixels along the same axis.
+    This method can only generate (hyper)rectangular stencils. To create the same estimator as
+    in the previous examples, use here
+
+    ```julia
+    stencil = ((2, 2), (1, 1))
+    ```
+    When passing a stencil using `extent` and `lag`, `m = prod(extent)!`.
 
 After having defined `est`, one calculates the spatial permutation entropy
 by calling [`entropy`](@ref) with `est`, and with the array data.
@@ -55,11 +87,10 @@ struct SpatialSymbolicPermutation{D,P,V} <: ProbabilitiesEstimator
     valid::V
 end
 function SpatialSymbolicPermutation(
-        stencil::Vector{CartesianIndex{D}}, x::AbstractArray, p::Bool = true
-    ) where {D}
-    # Ensure that no offset is part of the stencil
-    stencil = pushfirst!(copy(stencil), CartesianIndex{D}(zeros(Int, D)...))
+        stencil, x::AbstractArray, p::Bool = true
+    )
     arraysize = size(x)
+    stencil, D = stencil_to_offsets(stencil)
     @assert length(arraysize) == D "Indices and input array must match dimensionality!"
     # Store valid indices for later iteration
     if p
@@ -75,6 +106,28 @@ function SpatialSymbolicPermutation(
         valid = Base.Generator(idxs -> CartesianIndex{D}(idxs), ranges)
     end
     SpatialSymbolicPermutation{D, p, typeof(valid)}(stencil, copy(stencil), arraysize, valid)
+end
+
+# get stencil in the form of vectors of cartesian indices from either input type
+stencil_to_offsets(stencil::Vector{CartesianIndex{D}}) where D = stencil, D
+
+function stencil_to_offsets(stencil::NTuple{2, NTuple{D, T}}) where {D, T}
+    # get extent and lag from stencil
+    extent, lag = stencil
+    # generate a D-dimensional stencil
+    # start by generating a list of iterators for each dimension
+    iters = [0:lag[i]:extent[i]-1 for i in 1:D]
+    # then generate the stencil. We use an iterator product that we basically only reshape after that
+    stencil = CartesianIndex.(vcat(collect(Iterators.product(iters...))...))
+    return stencil, D
+end
+
+function stencil_to_offsets(stencil::Array{Int, D}) where D
+    # translate D-dim array into stencil of cartesian indices (of dimension D)
+    stencil = [idx - CartesianIndex(Tuple(ones(Int, D))) for idx in findall(Bool.(stencil))]
+    # subtract first coordinate from everything to get a stencil that contains (0,0)
+    stencil = [idx - stencil[1] for idx in stencil]
+    return stencil, D
 end
 
 # This source code is a modification of the code of Agents.jl that finds neighbors
