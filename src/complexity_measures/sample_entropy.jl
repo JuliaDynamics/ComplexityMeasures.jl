ab55130d39df5ab16a0ec3c3ccbc6e44bc5a1951
Kristian Haaga <kahaaga@gmail.com>
2022-10-22 13:42:26 +0200

    Add convencience wrapper, as agreed on in #130

--- src/complexity_measures/sample_entropy.jl
+++ src/complexity_measures/sample_entropy.jl
@@ -150,9 +150,14 @@ function complexity_normalized(c::SampleEntropy, x::AbstractVector{T}) where T <
 end
 
 """
-    sample_entropy(x, r; m = 2, τ = 1, metric = Chebyshev(), normalize = true)
+    sample_entropy(x; r, m = 2, τ = 1, metric = Chebyshev(), normalize = true)
 
-Convenience syntax for estimating the sample entropy of timeseries `x`.
+Convenience syntax for estimating the (normalized) sample entropy (Richman & Moorman, 2000)
+of timeseries `x`.
+
+This is just a wrapper for `complexity(SampleEntropy(; r, m, τ, base), x)`.
+
+See also: [`SampleEntropy`](@ref), [`complexity`](@ref), [`complexity_normalized`](@ref)).
 """
 function sample_entropy(x, r; m = 2, τ = 1, metric = Chebyshev(), normalize = true)
     c = SampleEntropy(x; r, m, τ, metric)
