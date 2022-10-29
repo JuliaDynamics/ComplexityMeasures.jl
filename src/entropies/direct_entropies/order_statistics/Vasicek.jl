a02299d584fbb687f5147767cf832bb2ffcdf4e6
Kristian Haaga <kahaaga@gmail.com>
2022-10-29 17:38:48 +0200

    Remove duplicate code

--- src/entropies/direct_entropies/order_statistics/Vasicek.jl
+++ src/entropies/direct_entropies/order_statistics/Vasicek.jl
@@ -35,22 +35,6 @@ written for this package).
     base::B = 2
 end
 
-"""
-    ith_order_statistic(ex, i::Int, n::Int = length(x))
-
-Return the i-th order statistic from the order statistics `ex`, requiring that
-`Xᵢ = X₁` if `i < 1` and `Xᵢ = Xₙ` if `i > n`.
-"""
-function ith_order_statistic(ex, i::Int, n::Int = length(x))
-    if i < 1
-        return ex[1]
-    elseif i > n
-        return ex[n]
-    else
-        return ex[i]
-    end
-end
-
 function entropy(e::Vasicek, x::AbstractVector{T}) where T
     (; m, base) = e
     n = length(x)
