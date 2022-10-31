9daf858fd628244c22441dabcf38cc087fec2051
Kristian Haaga <kahaaga@gmail.com>
2022-10-31 12:09:10 +0100

    Move ComplexityMeasures to separate package

--- docs/toc.jl
+++ docs/toc.jl
@@ -2,7 +2,6 @@ ENTROPIES_PAGES = [
     "Entropies.jl" => "index.md",
     "Probabilities" => "probabilities.md",
     "Entropies" => "entropies.md",
-    "Complexity measures" => "complexity_measures.md",
     "Entropies.jl examples" => "examples.md",
     "Utility methods" => "utils.md",
 ]
--- test/complexity_measures/complexity_measures.jl
+++
@@ -1,8 +0,0 @@
-using Entropies, Test
-
-@testset "Complexity measures" begin
-    testfile("./reverse_dispersion.jl")
-    testfile("./missing_dispersion.jl")
-    testfile("./approx_entropy.jl")
-    testfile("./sample_entropy.jl")
-end
