b4a0db010d5415a035f766a898583377a735eaa1
Datseris <datseris.george@gmail.com>
2022-10-31 10:18:08 +0000

    move all entropyers to maximum_entropy

--- src/entropies/entropies.jl
+++ src/entropies/entropies.jl
@@ -4,4 +4,3 @@ include("curado.jl")
 include("streched_exponential.jl")
 include("convenience_definitions.jl")
 include("direct_entropies/nearest_neighbors/nearest_neighbors.jl")
-# TODO: What else is included here from direct entropies?
