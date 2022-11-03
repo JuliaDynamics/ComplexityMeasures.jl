09a1d1842e5661423caa9ca491df1432ae171731
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2022-11-03 10:37:28 +0100

    Indirect Zhu et al. kNN Shannon entropy estimator for univariate/multivariate data (#156)

--- test/entropies/nearest_neighbors_direct.jl
+++ test/entropies/nearest_neighbors_direct.jl
@@ -46,3 +46,24 @@ end
     @test h_XN_base_e - tol_e ≤ h_XN_kr_base_e ≤ h_XN_base_e + tol_e
     @test h_XN_base_2 - tol_2 ≤ h_XN_kr_base_2 ≤ h_XN_base_2 + tol_2
 end
+
+@testset "NN - Zhu" begin
+
+    # To ensure minimal rectangle volumes are correct, we also test internals directly here.
+    # It's not feasible to construct an end-product test due to the neighbor searches.
+    x = Dataset([[-1, -2], [0, -2], [3, 2]])
+    y = Dataset([[3, 1], [-5, 1], [3, -2]])
+    @test Entropies.volume_minirect([0, 0], x) == 24
+    @test Entropies.volume_minirect([0, 0], y) == 40
+
+    # Analytical tests for the estimated entropy
+    DN = Dataset(randn(200000, 1))
+    hN_base_e = 0.5 * log(MathConstants.e, 2π) + 0.5
+    hN_base_2 = hN_base_e / log(2, MathConstants.e)
+
+    e_base_e = Zhu(k = 3, base = MathConstants.e)
+    e_base_2 = Zhu(k = 3, base = 2)
+
+    @test round(entropy(e_base_e, DN), digits = 1) == round(hN_base_e, digits = 1)
+    @test round(entropy(e_base_2, DN), digits = 1) == round(hN_base_2, digits = 1)
+end
