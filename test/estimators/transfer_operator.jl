cce1c150f7905bc0a98193246daf4362556f74cc
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2022-11-17 11:35:10 +0100

    Fix `TransferOperator` (#178)
    * Vector of outcomes
    
    * Fix outcomes
    
    * Update tests
    
    * Allow both regular and fixed binnings
    
    * Update docs
    
    * Comment on ordering of bins

--- test/estimators/transfer_operator.jl
+++ test/estimators/transfer_operator.jl
@@ -1,13 +1,31 @@
 @test TransferOperator(RectangularBinning(3)) isa TransferOperator
 
-D = Dataset(rand(1000, 3))
+D = Dataset(rand(100, 2))
 
 binnings = [
     RectangularBinning(3),
     RectangularBinning(0.2),
-    RectangularBinning([2, 2, 3]),
-    RectangularBinning([0.2, 0.3, 0.3])
+    RectangularBinning([2, 3]),
+    RectangularBinning([0.2, 0.3]),
 ]
+
+# There's not easy way of constructing an analytical example for the resulting
+# probabilities, because they are computed according to the approximation to the
+# transfer operator, not as naive visitation freqiencies to the bins. We just test
+# generically here.
+# TODO: make a stupidly simple example where we can actually compute the measure of
+# each bin exactly.
+fb = FixedRectangularBinning(0, 1, 2)
+@test total_outcomes(D, fb) == 2^2
+@test outcome_space(D, fb) ≈ SVector.([(0.0, 0.0), (0.5, 0.0), (0.0, 0.5), (0.5, 0.5)])
+
+# For non-fixed binnings, an error should be thrown for both `total_outcomes` and
+# `outcome_space`.
+@testset "Binning test $i" for i in eachindex(binnings)
+    @test_throws ErrorException total_outcomes(D, binnings[i]) == 2^2
+    @test_throws ErrorException outcome_space(D, binnings[i]) == 2^2
+
+end
 
 @testset "Binning test $i" for i in eachindex(binnings)
     to = Entropies.transferoperator(D, binnings[i])
@@ -20,5 +38,7 @@ binnings = [
     @test p isa Probabilities
     @test bins isa Vector{<:SVector}
 
-    @test probabilities(D, TransferOperator(binnings[i])) isa Probabilities
+    est = TransferOperator(binnings[i])
+    @test probabilities(D, est) isa Probabilities
+    @test probabilities_and_outcomes(D, est) isa Tuple{Probabilities, Vector{SVector{2, Float64}}}
 end
