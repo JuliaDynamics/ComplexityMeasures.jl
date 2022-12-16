0f61496ad2f63e9e43282c6b9f167b7ab2d3d779
HeineRugland <heine.rugland@gmail.com>
2022-12-16 15:53:37 +0100

    Added test for line 34.

--- test/entropies/entropy_types/kaniadakis.jl
+++ test/entropies/entropy_types/kaniadakis.jl
@@ -2,11 +2,11 @@ N = 10
 κs = [0.5, 1.0, 2.0, 5.0, 10.0]
 
 probs = Probabilities([1, 3, 6,2, 9])
-
 @test entropy(Kaniadakis(), probs) isa Real
-
 @test entropy(Kaniadakis(), probs) >= 0
-
 @test entropy(Kaniadakis(κ = 0, base = 2), probs) == entropy(Shannon(base = 2), probs)
+
+p = Probabilities([0, 1])
+@test entropy(Kaniadakis(), p) == 0
 
 # Kaniadakis does not state for which distribution for which Kaniadakis entropy is maximised
