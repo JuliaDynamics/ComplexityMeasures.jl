f08cd34e5e6d7b2de844d5a9363607c3ba37bf12
George Datseris <datseris.george@gmail.com>
2022-12-19 12:24:42 +0200

    reverse order in encode/decode

--- src/encodings.jl
+++ src/encodings.jl
@@ -17,16 +17,16 @@ Current available encodings are:
 abstract type Encoding end
 
 """
-    encode(χ, c::Encoding) -> i::Int
+    encode(c::Encoding, χ) -> i::Int
 Encoding an element `χ ∈ x` of input data `x` (those given to [`probabilities`](@ref))
 using encoding `c`. The special value of `-1` is reserved as a return value for
-inappropriate elements `χ` that cannot be encoded according to `e`.
+inappropriate elements `χ` that cannot be encoded according to `c`.
 """
 function encode end
 
 """
-    decode(i::Int, c::Encoding) -> ω
-Decode an encoded element `i::Int` into the outcome it corresponds to `ω ∈ Ω`.
+    decode(c::Encoding, i::Int) -> ω
+Decode an encoded element `i` into the outcome it corresponds to `ω ∈ Ω`.
 `Ω` is the [`outcome_space`](@ref) of a probabilities estimator that uses encoding `c`.
 """
 function decode end
