3af51fadba7f754876690e0cc2b796f21ccc4fda
Kristian Haaga <kahaaga@gmail.com>
2022-12-26 23:00:12 +0100

    Fix tests for non-exported stuff

--- test/complexity/multiscale.jl
+++ test/complexity/multiscale.jl
@@ -3,10 +3,10 @@ using Entropies
 x = rand(1000)
 c = ReverseDispersion()
 maxscale = 5
-mcr = multiscale(Regular(), c, x; maxscale)
-mcc = multiscale(Composite(), c, x; maxscale)
-mcrn = multiscale_normalized(Regular(), c, x; maxscale)
-mccn = multiscale_normalized(Composite(), c, x; maxscale)
+mcr = Entropies.multiscale(Regular(), c, x; maxscale)
+mcc = Entropies.multiscale(Composite(), c, x; maxscale)
+mcrn = Entropies.multiscale_normalized(Regular(), c, x; maxscale)
+mccn = Entropies.multiscale_normalized(Composite(), c, x; maxscale)
 @test mcr isa Vector{T} where T <: Real
 @test mcc isa Vector{T} where T <: Real
 @test mcrn isa Vector{T} where T <: Real
--- test/multiscale/downsampling.jl
+++ test/multiscale/downsampling.jl
@@ -7,22 +7,22 @@ x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 r = Regular(f = mean)
 
 # just returns original time series
-@test downsample(r, 1, x) == x
+@test Entropies.downsample(r, 1, x) == x
 
 # for scale 2, downsampling indices for `x` are 1:2, 3:4, 5:6, 7:8, 9:10, so we should get
 # the means of consecutive running pairs
-@test downsample(r, 2, x) == [1.5, 3.5, 5.5, 7.5, 9.5]
+@test Entropies.downsample(r, 2, x) == [1.5, 3.5, 5.5, 7.5, 9.5]
 
 # for scale 3, downsampling indices for `x` are 1:3, 4:6, 7:9. Take the means:
-@test downsample(r, 3, x) == [2.0, 5.0, 8.0]
+@test Entropies.downsample(r, 3, x) == [2.0, 5.0, 8.0]
 
 # for scale 4, downsampling indices for `x` are 1:4, 5:8. Take the means:
-@test downsample(r, 4, x) == [2.5, 6.5]
+@test Entropies.downsample(r, 4, x) == [2.5, 6.5]
 
 # for scale 5, downsampling indices for `x` are 1:5, 6:10. Take the means:
-@test downsample(r, 5, x) == [3.0, 8.0]
+@test Entropies.downsample(r, 5, x) == [3.0, 8.0]
 
-@test_throws DomainError downsample(r, 6, x)
+@test_throws DomainError Entropies.downsample(r, 6, x)
 
 ##############################################################
 # Regular downsampling
@@ -30,24 +30,24 @@ r = Regular(f = mean)
 c = Composite(f = mean)
 # for scale 2, downsampling indices for `x` are 1:2, 3:4, 5:6, 7:8, 9:10, so we should get
 # the means of consecutive running pairs
-@test downsample(c, 2, x) == [
+@test Entropies.downsample(c, 2, x) == [
     [1.5, 3.5, 5.5, 7.5], # indices 1:2, 3:4, 5:6, 7:8
     [2.5, 4.5, 6.5, 8.5], # indices 2:3, 4:5, 6:7, 8:9
 ]
-@test downsample(c, 3, x) == [
+@test Entropies.downsample(c, 3, x) == [
     [2.0, 5.0], # indices 1:3, 4:6
     [3.0, 6.0], # indices 2:4, 5:7
     [4.0, 7.0], # indices 3:5, 6:8
 ]
 
-@test downsample(c, 4, x) == [
+@test Entropies.downsample(c, 4, x) == [
     [2.5], # indices 1:4
     [3.5], # indices 2:5
     [4.5], # indices 3:6
     [5.5], # indices 4:7
 ]
 
-@test downsample(c, 5, x) == [
+@test Entropies.downsample(c, 5, x) == [
     [3.0], # indices 1:5
     [4.0], # indices 2:6
     [5.0], # indices 3:7
@@ -55,4 +55,4 @@ c = Composite(f = mean)
     [7.0], # indices 5:9
 ]
 
-@test_throws DomainError downsample(c, 6, x)
+@test_throws DomainError Entropies.downsample(c, 6, x)
