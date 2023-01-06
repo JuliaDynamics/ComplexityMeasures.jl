3d1d5073e594e2017fc72e28d398d3e38ceaa83d
George Datseris <datseris.george@gmail.com>
2023-01-06 12:33:54 +0200

    check if base is same in compatibility

--- src/entropies_estimators/entropies_estimators.jl
+++ src/entropies_estimators/entropies_estimators.jl
@@ -3,7 +3,16 @@
 Return `true` if the given `est` can estimate the given definition of `e`,
 otherwise throw an `ArgumentError`.
 """
-entropy_definition_compatibility(::Shannon, est::DiffEntropyEst) = true
+function entropy_definition_compatibility(e::Shannon, est::DiffEntropyEst)
+    if e.base != est.base
+        throw(ArgumentError(
+            "While the estimator is compatible with the Shannon entropy, "*
+            "the Shannon definition and the estimator have different base."
+        ))
+    end
+    return true
+end
+
 function entropy_definition_compatibility(e::EntropyDefinition, est::DiffEntropyEst)
     t = string(nameof(typeof(e)))
     throw(ArgumentError("$t entropy not implemented for $(typeof(est)) estimator"))
