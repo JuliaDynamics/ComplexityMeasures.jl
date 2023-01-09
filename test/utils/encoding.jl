210885067c7488097645f4421822d04ccd88ffa8
Kristian Haaga <kahaaga@gmail.com>
2023-01-09 14:50:08 +0100

    Comment binning-stuff. Should we introduce this later?

--- test/utils/encoding.jl
+++ test/utils/encoding.jl
@@ -84,138 +84,138 @@ end
     end
 end
 
-@testset "Equal-width intervals (rectangular binning)" begin
-    N = 10 # each dimension is divides [min, max] into N chunks
-    @testset "User-defined grid" begin
-        # For datasets
-        # --------------------------------
-        D = Dataset([-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0])
-        r = (1 - (-1)) / N
+# @testset "Equal-width intervals (rectangular binning)" begin
+#     N = 10 # each dimension is divides [min, max] into N chunks
+#     @testset "User-defined grid" begin
+#         # For datasets
+#         # --------------------------------
+#         D = Dataset([-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0])
+#         r = (1 - (-1)) / N
 
-        # (-1, 1) range for all dims.
-        binning_same = FixedRectangularBinning(-1.0, 1.0, N)
+#         # (-1, 1) range for all dims.
+#         binning_same = FixedRectangularBinning(-1.0, 1.0, N)
 
-        # (axismins[1], axismaxs[1]) gives the range for the 1st dim.
-        axismins, axismaxs = (-1.0, -1.0), (1.0, 1.0)
-        binning_diff = FixedRectangularBinning(axismins, axismaxs, N)
+#         # (axismins[1], axismaxs[1]) gives the range for the 1st dim.
+#         axismins, axismaxs = (-1.0, -1.0), (1.0, 1.0)
+#         binning_diff = FixedRectangularBinning(axismins, axismaxs, N)
 
-        # Verify that grids are as expected.
-        symb_diff = RectangularBinEncoding(D, binning_diff)
-        symb_same = RectangularBinEncoding(D, binning_same)
-        @test all(@. symb_diff.edgelengths ≈ r)
-        @test all(@. symb_same.edgelengths ≈ r)
-        @test all(@. symb_diff.mini ≈ -1.0)
-        @test all(@. symb_same.mini ≈ -1.0)
+#         # Verify that grids are as expected.
+#         symb_diff = RectangularBinEncoding(D, binning_diff)
+#         symb_same = RectangularBinEncoding(D, binning_same)
+#         @test all(@. symb_diff.edgelengths ≈ r)
+#         @test all(@. symb_same.edgelengths ≈ r)
+#         @test all(@. symb_diff.mini ≈ -1.0)
+#         @test all(@. symb_same.mini ≈ -1.0)
 
-        # bins are indexed from 0, so should get the following symbols
-        expected = [SVector(0,0), SVector(4, 4), SVector(9, 9)]
-        @test outcomes(D, symb_diff) ==
-            outcomes(D, symb_same) ==
-            expected
+#         # bins are indexed from 0, so should get the following symbols
+#         expected = [SVector(0,0), SVector(4, 4), SVector(9, 9)]
+#         @test outcomes(D, symb_diff) ==
+#             outcomes(D, symb_same) ==
+#             expected
 
-        @test total_outcomes(symb_diff) == N^2
-        @test total_outcomes(symb_same) == N^2
-        @test total_outcomes(D, symb_diff) == N^2
-        @test total_outcomes(D, symb_same) == N^2
+#         @test total_outcomes(symb_diff) == N^2
+#         @test total_outcomes(symb_same) == N^2
+#         @test total_outcomes(D, symb_diff) == N^2
+#         @test total_outcomes(D, symb_same) == N^2
 
-        # all possible outcomes
-        o = outcome_space(symb_diff)
-        @test length(o) == N^2
-        @test eltype(o) == SVector{2, Int}
-        for i in 1:N
-            for j in 1:N
-                @test SVector(i, j) ∈ o
-            end
-        end
+#         # all possible outcomes
+#         o = outcome_space(symb_diff)
+#         @test length(o) == N^2
+#         @test eltype(o) == SVector{2, Int}
+#         for i in 1:N
+#             for j in 1:N
+#                 @test SVector(i, j) ∈ o
+#             end
+#         end
 
-        # For univariate timeseries
-        # --------------------------------
-        # bins are indexed from 0, so should get [0, 4, 9] with N = 10
-        x = [-1.0, 0.0, 1.0]
-        symb_1D = RectangularBinEncoding(x, binning_same)
+#         # For univariate timeseries
+#         # --------------------------------
+#         # bins are indexed from 0, so should get [0, 4, 9] with N = 10
+#         x = [-1.0, 0.0, 1.0]
+#         symb_1D = RectangularBinEncoding(x, binning_same)
 
-        @test total_outcomes(symb_1D) == N
-        @test total_outcomes(x, symb_1D) == N
-        @test symb_1D.mini ≈ -1.0
-        @test symb_1D.edgelengths ≈ (1 - (-1)) / N
-        @test outcomes(x, symb_1D) == [0, 4, 9]
-    end
+#         @test total_outcomes(symb_1D) == N
+#         @test total_outcomes(x, symb_1D) == N
+#         @test symb_1D.mini ≈ -1.0
+#         @test symb_1D.edgelengths ≈ (1 - (-1)) / N
+#         @test outcomes(x, symb_1D) == [0, 4, 9]
+#     end
 
-    @testset "Grid defined by data" begin
-        D = Dataset([-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0])
-        r = nextfloat((1.0 - (-1.0)) / N)
-        binning_int = RectangularBinning(N)
-        binning_intvec = RectangularBinning([N, N])
-        binning_float = RectangularBinning(r)
-        binning_floatvec = RectangularBinning([r, r])
+#     @testset "Grid defined by data" begin
+#         D = Dataset([-1.0, 0.0, 1.0], [-1.0, 0.0, 1.0])
+#         r = nextfloat((1.0 - (-1.0)) / N)
+#         binning_int = RectangularBinning(N)
+#         binning_intvec = RectangularBinning([N, N])
+#         binning_float = RectangularBinning(r)
+#         binning_floatvec = RectangularBinning([r, r])
 
-        symb_int = RectangularBinEncoding(D, binning_int)
-        symb_float = RectangularBinEncoding(D, binning_float)
-        symb_intvec = RectangularBinEncoding(D, binning_intvec)
-        symb_floatvec = RectangularBinEncoding(D, binning_floatvec)
+#         symb_int = RectangularBinEncoding(D, binning_int)
+#         symb_float = RectangularBinEncoding(D, binning_float)
+#         symb_intvec = RectangularBinEncoding(D, binning_intvec)
+#         symb_floatvec = RectangularBinEncoding(D, binning_floatvec)
 
-        @test round.(symb_int.edgelengths, digits = 15) ==
-            round.(symb_float.edgelengths, digits = 15) ==
-            round.(symb_intvec.edgelengths, digits = 15) ==
-            round.(symb_floatvec.edgelengths, digits = 15)
-        @test all(@. symb_int.edgelengths ≈ r)
-        @test all(@. symb_float.edgelengths ≈ r)
-        @test all(@. symb_intvec.edgelengths ≈ r)
-        @test all(@. symb_floatvec.edgelengths ≈ r)
-        @test all(@. symb_int.mini ≈ -1.0)
-        @test all(@. symb_float.mini ≈ -1.0)
-        @test all(@. symb_intvec.mini ≈ -1.0)
-        @test all(@. symb_floatvec.mini ≈ -1.0)
+#         @test round.(symb_int.edgelengths, digits = 15) ==
+#             round.(symb_float.edgelengths, digits = 15) ==
+#             round.(symb_intvec.edgelengths, digits = 15) ==
+#             round.(symb_floatvec.edgelengths, digits = 15)
+#         @test all(@. symb_int.edgelengths ≈ r)
+#         @test all(@. symb_float.edgelengths ≈ r)
+#         @test all(@. symb_intvec.edgelengths ≈ r)
+#         @test all(@. symb_floatvec.edgelengths ≈ r)
+#         @test all(@. symb_int.mini ≈ -1.0)
+#         @test all(@. symb_float.mini ≈ -1.0)
+#         @test all(@. symb_intvec.mini ≈ -1.0)
+#         @test all(@. symb_floatvec.mini ≈ -1.0)
 
-        @test outcomes(D, symb_int) ==
-            outcomes(D, symb_intvec) ==
-            outcomes(D, symb_float) ==
-            outcomes(D, symb_floatvec) ==
-            [SVector(0,0), SVector(4,4), SVector(9,9)]
+#         @test outcomes(D, symb_int) ==
+#             outcomes(D, symb_intvec) ==
+#             outcomes(D, symb_float) ==
+#             outcomes(D, symb_floatvec) ==
+#             [SVector(0,0), SVector(4,4), SVector(9,9)]
 
-        x = [-1.0, 0.0, 1.0]
-        r = (1.0 - (-1.0)) / N
-        binning_int = RectangularBinning(N)
-        binning_intvec = RectangularBinning([N])
-        symb_int = RectangularBinEncoding(x, binning_int)
-        symb_float = RectangularBinEncoding(x, binning_float)
+#         x = [-1.0, 0.0, 1.0]
+#         r = (1.0 - (-1.0)) / N
+#         binning_int = RectangularBinning(N)
+#         binning_intvec = RectangularBinning([N])
+#         symb_int = RectangularBinEncoding(x, binning_int)
+#         symb_float = RectangularBinEncoding(x, binning_float)
 
-        @test all(@. symb_int.edgelengths ≈ r)
-        @test all(@. symb_float.edgelengths ≈ r)
-        @test all(@. symb_int.mini ≈ -1.0)
-        @test all(@. symb_float.mini ≈ -1.0)
+#         @test all(@. symb_int.edgelengths ≈ r)
+#         @test all(@. symb_float.edgelengths ≈ r)
+#         @test all(@. symb_int.mini ≈ -1.0)
+#         @test all(@. symb_float.mini ≈ -1.0)
 
-        @test outcomes(x, symb_int) ==
-            outcomes(x, symb_float) ==
-            [0, 4, 9]
-    end
+#         @test outcomes(x, symb_int) ==
+#             outcomes(x, symb_float) ==
+#             [0, 4, 9]
+#     end
 
-    @testset "outcome length" begin
-        X = Dataset(rand(10, 3))
-        x = rand(10)
-        rbN = RectangularBinning(5)
-        rbNs = RectangularBinning([5, 3, 4])
-        rbF = RectangularBinning(0.5)
-        rbFs = RectangularBinning([0.5, 0.4, 0.3])
+#     @testset "outcome length" begin
+#         X = Dataset(rand(10, 3))
+#         x = rand(10)
+#         rbN = RectangularBinning(5)
+#         rbNs = RectangularBinning([5, 3, 4])
+#         rbF = RectangularBinning(0.5)
+#         rbFs = RectangularBinning([0.5, 0.4, 0.3])
 
-        symbolization_xN = RectangularBinEncoding(x, rbN)
-        symbolization_xF = RectangularBinEncoding(x, rbF)
-        symbolization_XN = RectangularBinEncoding(X, rbN)
-        symbolization_XNs = RectangularBinEncoding(X, rbNs)
-        symbolization_XF = RectangularBinEncoding(X, rbF)
-        symbolization_XFs = RectangularBinEncoding(X, rbFs)
+#         symbolization_xN = RectangularBinEncoding(x, rbN)
+#         symbolization_xF = RectangularBinEncoding(x, rbF)
+#         symbolization_XN = RectangularBinEncoding(X, rbN)
+#         symbolization_XNs = RectangularBinEncoding(X, rbNs)
+#         symbolization_XF = RectangularBinEncoding(X, rbF)
+#         symbolization_XFs = RectangularBinEncoding(X, rbFs)
 
-        @test total_outcomes(x, symbolization_xN) == 5
-        @test_throws ArgumentError total_outcomes(x, symbolization_xF)
-        @test_throws ArgumentError total_outcomes(x, symbolization_xF)
-        @test_throws ArgumentError total_outcomes(x, symbolization_XNs)
+#         @test total_outcomes(x, symbolization_xN) == 5
+#         @test_throws ArgumentError total_outcomes(x, symbolization_xF)
+#         @test_throws ArgumentError total_outcomes(x, symbolization_xF)
+#         @test_throws ArgumentError total_outcomes(x, symbolization_XNs)
 
-        @test total_outcomes(X, symbolization_XN) == 5^3
-        @test total_outcomes(X, symbolization_XNs) == 5*3*4
-        @test_throws ArgumentError total_outcomes(X, symbolization_XF)
-        @test_throws ArgumentError total_outcomes(X, symbolization_XFs)
-    end
-end
+#         @test total_outcomes(X, symbolization_XN) == 5^3
+#         @test total_outcomes(X, symbolization_XNs) == 5*3*4
+#         @test_throws ArgumentError total_outcomes(X, symbolization_XF)
+#         @test_throws ArgumentError total_outcomes(X, symbolization_XFs)
+#     end
+# end
 
 @testset "Missing symbols" begin
     x = [1, 2, 3, 4, 2, 1, 0]
