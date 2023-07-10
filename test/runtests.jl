0627fa41640f7c8e18c97077dc3a79841e88bfd4
Datseris <datseris.george@gmail.com>
2023-07-10 12:01:14 +0100

    actually include tests!

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,22 @@ using ComplexityMeasures
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 @testset "ComplexityMeasures.jl" begin
-    include("probabilities/probabilities.jl")
+    testfile("probabilities/interface.jl")
+    # Probability estimators.
+    testfile("probabilities/estimators/count_occurrences.jl")
+    testfile("probabilities/estimators/value_histogram.jl")
+    testfile("probabilities/estimators/transfer_operator.jl")
+    testfile("probabilities/estimators/naive_kernel.jl")
+    testfile("probabilities/estimators/permutation.jl")
+    testfile("probabilities/estimators/timescales.jl")
+    testfile("probabilities/estimators/dispersion.jl")
+    testfile("probabilities/estimators/diversity.jl")
+    testfile("probabilities/estimators/spatial/spatial_permutation.jl")
+    testfile("probabilities/estimators/spatial/spatial_dispersion.jl")
+    # probabilities functions
+    testfile("probabilities/allprobabilities.jl")
+
+
     include("entropies/entropies.jl")
     include("complexity/complexity.jl")
 
