57123d3319127661d2b3efd7fb715daa39caf906
Kristian Haaga <kahaaga@gmail.com>
2023-07-20 01:27:44 +0200

    Correct function names

--- test/infomeasures/infomeasure_types/identification.jl
+++ test/infomeasures/infomeasure_types/identification.jl
@@ -2,15 +2,15 @@
 # Analytical tests
 # -----------------------------------
 # Minimal and equal to zero when probability distribution has only one element...
-@test entropy(Identification(), Probabilities([1.0])) ≈ 0.0
+@test information(Identification(), Probabilities([1.0])) ≈ 0.0
 
 # or minimal when only one probability is nonzero and equal to 1.0
-@test entropy(Identification(), Probabilities([1.0, 0.0, 0.0, 0.0])) ≈ 0.0
+@test information(Identification(), Probabilities([1.0, 0.0, 0.0, 0.0])) ≈ 0.0
 
 # Maximal for a uniform distribution
 x = [0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.4, 0.4]
-@test entropy_normalized(Identification(), CountOccurrences(), x) ≈ 1.0
+@test information_normalized(Identification(), CountOccurrences(), x) ≈ 1.0
 
 # Submaximal for a non-uniform distribution
 y = [0.1, 0.2, 0.2, 0.25, 0.2, 0.351, 0.312, 0.3, 0.3, 0.4, 0.4]
-@test entropy_normalized(Identification(), CountOccurrences(), y) < 1.0
+@test information_normalized(Identification(), CountOccurrences(), y) < 1.0
