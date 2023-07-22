bc585630ee874a96a6205e55bad5c9c34763116c
Datseris <datseris.george@gmail.com>
2023-07-22 11:51:35 +0200

    mention densities in docstring of diffinfo

--- src/core/information_functions.jl
+++ src/core/information_functions.jl
@@ -47,3 +47,49 @@ end
 information(est::ProbabilitiesEstimator, x) = information(Shannon(), est, x)
 information(probs::Probabilities) = information(Shannon(), probs)
 information(e::PlugIn, probs::Probabilities) = information(e.definition, probs)
+
+
+# Differential
+"""
+    information(est::DifferentialInfoEstimator, x) → h::Real
+
+Approximate a **differential information measure** using the provided
+[`DifferentialInfoEstimator`](@ref) and input data `x`.
+
+The overwhelming majority of differential estimators estimate the [`Shannon`](@ref)
+entropy. If the same estimator can estimate different information measures (e.g. it can
+estimate both [`Shannon`](@ref) and [`Tsallis`](@ref)), then the information measure
+is provided as an argument to the estimator itself.
+
+See the [table of differential information measure estimators](@ref table_diff_ent_est)
+in the docs for all differential information measure estimators.
+
+Currently, unlike the discrete information measure this method doesn't involve explicitly
+first computing a probability density function and then passing this density
+to an information measure definition. But in the future, we want to establish a
+`density` API similar to the [`probabilities`](@ref) API.
+
+## Examples
+
+A normal distribution has a base-e Shannon differential entropy of
+`0.5*log(2π) + 0.5` nats.
+
+```julia
+est = Kraskov(k = 5, base = ℯ) # Base `ℯ` for nats.
+h = information(est, randn(2_000_000))
+abs(h - 0.5*log(2π) - 0.5) # ≈ 0.0001
+```
+"""
+function information(est::DifferentialInfoEstimator, args...) where I
+    throw(ArgumentError("`information` not implemented for $(nameof(typeof(est)))"))
+end
+
+# Dispatch for these functions is implemented in individual estimator files in
+# `differential_info_estimators/`.
+
+function information(::InformationMeasure, ::DifferentialInfoEstimator, args...)
+    throw(ArgumentError(
+        """`InformationMeasure` must be given as an argument to `est`, not to `information`.
+        """
+    ))
+end
