125d70bc1c042a66da3bd795ba62398c27823d9f
Kristian Haaga <kahaaga@gmail.com>
2023-07-26 10:47:56 +0200

    Improve docstring

--- src/discrete_info_estimators/horvitz_thompson.jl
+++ src/discrete_info_estimators/horvitz_thompson.jl
@@ -17,12 +17,19 @@ H_S^{HT} = -\\sum_i=1^M \\( \\dfrac{p_i \\log(p_i}{1 - (1 - p_i)^N} \\),
 ```
 
 where ``N`` is the sample size and ``M`` is the number of [`outcomes`](@ref).
+Given the true probability ``p_i`` of the ``i``-th outcome, ``1 - (1 - p_i)^N`` is the
+probability that the outcome appears at least once in a sample of size ``N`` (Arora et al.,
+2022). Dividing by this inclusion probability is a form of weighting, and compensates
+for situations where certain outcomes have so low probabilities that they are not
+often observed in a sample, for example in power-law distributions.
 
 [^Horvitz1952]:
     Horvitz, D. G., & Thompson, D. J. (1952). A generalization of sampling without
     replacement from a finite universe. Journal of the American statistical Association,
     47(260), 663-685.
-
+[^Arora2022]:
+    Arora, A., Meister, C., & Cotterell, R. (2022). Estimating the entropy of linguistic
+    distributions. arXiv preprint arXiv:2204.01469.
 """
 struct HorvitzThompson{I <: InformationMeasure} <: DiscreteInfoEstimator{I}
     measure::I
