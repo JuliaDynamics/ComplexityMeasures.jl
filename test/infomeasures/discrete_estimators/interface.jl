7ff0632ecd0151ef286bd3ca86a833886b8ff246
Kristian Haaga <kahaaga@gmail.com>
2023-07-29 23:15:50 +0200

    Discrete info estimators API

---
+++ test/infomeasures/discrete_estimators/interface.jl
@@ -0,0 +1,34 @@
+using Random
+rng = Random.MersenneTwister(1234)
+
+probests = [
+    Dispersion(),
+    SymbolicPermutation(),
+    SymbolicAmplitudeAwarePermutation(),
+    SymbolicWeightedPermutation(),
+    WaveletOverlap(),
+    PowerSpectrum(),
+    ValueHistogram(RectangularBinning(3)),
+    Diversity()
+]
+h = Shannon()
+hests = [
+    ChaoShen(h),
+    HorvitzThompson(h),
+    MillerMadow(h),
+    Schürmann(h),
+    GeneralizedSchürmann(h),
+    JackknifeEstimator(h),
+]
+
+x = rand(rng, 300)
+for pest in probests
+    hplugin = information(PlugIn(Shannon()), pest, x)
+
+    for hest in hests
+        h = information(hest, pest, x)
+        @test h isa Real
+        @test h >= 0
+        @test h > hplugin || abs(h - hplugin) < 1e-5
+    end
+end
