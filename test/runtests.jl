2f5552f2088fec75b28af701e67da9666618c68d
Kristian Haaga <kahaaga@gmail.com>
2023-08-11 12:17:23 +0200

    Test outcome spaces

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,9 @@ using ComplexityMeasures
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 @testset "ComplexityMeasures.jl" begin
+    # Outcome spaces
+    testfile("outcome_spaces/outcome_spaces.jl")
+
     # Probability estimators
     testfile("probabilities/estimators/count_occurrences.jl")
     testfile("probabilities/estimators/value_histogram.jl")
