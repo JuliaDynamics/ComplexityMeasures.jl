6285410c77ffeb117d1adab4eaf45b8ea27d903c
Kristian Haaga <kahaaga@gmail.com>
2023-08-15 15:59:20 +0200

    AddConstant probabilities estimator

--- src/probabilities_estimators/probabilities_estimators.jl
+++ src/probabilities_estimators/probabilities_estimators.jl
@@ -1,6 +1,8 @@
 include("MLE.jl")
 include("Bayes.jl")
 include("Shrinkage.jl")
+include("AddConstant.jl")
+
 # These don't actually result in probability distributions.
 #include("SampleCoverage.jl")
 #include("GoodTuring.jl")
