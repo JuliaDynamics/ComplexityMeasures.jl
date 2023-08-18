f3ed7617474d15d982d001fffffd427c9fc9cbbc
Kristian Haaga <kahaaga@gmail.com>
2023-08-18 09:23:28 +0200

    Move tests to correct folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,20 +6,18 @@ testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include
 @testset "ComplexityMeasures.jl" begin
     # Outcome spaces
     testfile("outcome_spaces/outcome_spaces.jl")
+    testfile("outcome_spaces/implementations/count_occurrences.jl")
+    testfile("outcome_spaces/implementations/value_histogram.jl")
+    testfile("outcome_spaces/implementations/transfer_operator.jl")
+    testfile("outcome_spaces/implementations/naive_kernel.jl")
+    testfile("outcome_spaces/implementations/permutation.jl")
+    testfile("outcome_spaces/implementations/timescales.jl")
+    testfile("outcome_spaces/implementations/dispersion.jl")
+    testfile("outcome_spaces/implementations/diversity.jl")
+    testfile("outcome_spaces/implementations/spatial/spatial_permutation.jl")
+    testfile("outcome_spaces/implementations/spatial/spatial_dispersion.jl")
 
-    # Probability estimators
-    testfile("probabilities/estimators/count_occurrences.jl")
-    testfile("probabilities/estimators/value_histogram.jl")
-    testfile("probabilities/estimators/transfer_operator.jl")
-    testfile("probabilities/estimators/naive_kernel.jl")
-    testfile("probabilities/estimators/permutation.jl")
-    testfile("probabilities/estimators/timescales.jl")
-    testfile("probabilities/estimators/dispersion.jl")
-    testfile("probabilities/estimators/diversity.jl")
-    testfile("probabilities/estimators/spatial/spatial_permutation.jl")
-    testfile("probabilities/estimators/spatial/spatial_dispersion.jl")
-    # probabilities functions
-    testfile("probabilities/api.jl")
+    # probabilities
     testfile("probabilities_estimators/probabilities_estimators.jl")
 
     include("infomeasures/infomeasures.jl")
