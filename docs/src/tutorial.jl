c25997b859fbdfb9ce05b9ae8e6d90fcd19bbcd9
Datseris <datseris.george@gmail.com>
2023-08-19 10:39:58 +0100

    add discrete info section to tutorial

--- docs/src/tutorial.jl
+++ docs/src/tutorial.jl
@@ -19,14 +19,16 @@
 # These complexity measures can highlight some aspects of the dynamics more than others, or distinguish one type of dynamics from another, or classify timeseries into classes with different dynamics, among other things.
 # ComplexityMeasures.jl implements hundreds such measures and hence it is named as such. To enable this, ComplexityMeasures.jl is more than a collection "dynamic statistics": it is also a framework for rigorously defining probability spaces and estimating probabilities from input data.
 
-# Within the documentation and codebase of ComplexityMeasures.jl we have taken the pragmatic choice to label all complexity measures that are _explicit functionals of probability mass or probability density functions_ as **information measures**, even though they might not be labelled as information measures in the literature. We use the term **complexity measure** for other quantities that are not explicit functionals of probabilities.
-# We stress that the separation between **information measure** and **complexity measure** is purely pragmatic, to establish a generic and extendable software interface within ComplexityMeasures.jl.
+# Within the codebase of ComplexityMeasures.jl we make a separation with the functions [`information`](@ref) (or its daughter function [`entropy`](@ref)) and [`complexity`](@ref).
+# We use `information` for complexity measures that are _explicit functionals of probability mass or probability density functions_, even though these measures might not be labelled as "information measures" in the literature.
+# We use `complexity` for other complexity measures that are not explicit functionals of probabilities.
+# We stress that the separation between `information` and `complexity` is purely pragmatic, to establish a generic and extendable software interface within ComplexityMeasures.jl.
 
 # ## The basis: Probabilities and Outcome Spaces
 
 # Information measures and some other complexity measures are computed based on **probabilities** derived from input data.
 # In order to derive probabilities from data, an **outcome space** (also called a sample space) needs to be defined: a way to transform data into elements $\omega$ of an outcome space $\omega \in \Omega$, and assign probabilities to each outcome $p(\omega)$, such that $p(\Omega)=1$. $\omega$ are called _outcomes_ or _events_.
-# In code, outcome spaces are instances of subtypes of [`OutcomeSpace`](@ref).
+# In code, outcome spaces are subtypes of [`OutcomeSpace`](@ref).
 # For example, one outcome space is the [`ValueHistogram`](@ref), which is the most commonly known outcome space, and corresponds to discretizing data by putting the data values into bins of a specific size.
 
 using ComplexityMeasures
@@ -36,11 +38,11 @@ x = randn(10_000)
 o = ValueHistogram(ε)
 o isa OutcomeSpace
 
-# such instances of outcome spaces may be given to [`probabilities`](@ref) to estimate the corresponding probabilities.
+# such outcome spaces may be given to [`probabilities`](@ref) to estimate the corresponding probabilities.
 
 probs = probabilities(o, x)
 
-# In this example the probabilities are the (normalized) heights of each bin of the histogram. However, we don't know the bins, which are the elements of the outcome space. To obtain the probabilities and the bins we would use
+# In this example the probabilities are the (normalized) heights of each bin of the histogram. However, we don't know the bins, which are the _elements_ of the outcome space. To obtain the probabilities and the bins we would use
 
 probs, outs = probabilities_and_outcomes(o, x)
 outs
@@ -51,14 +53,16 @@ using CairoMakie
 left_edges = first.(outs) # covert `Vector{SVector}` into `Vector{Real}`
 barplot(left_edges, probs; axis = (ylabel = "probability",))
 
-# Naturally, there are other outcome spaces one may use. A prominent example used in the NLTS literature are ordinal patterns.
+# Naturally, there are other outcome spaces one may use, and one can find the list of implemented ones in [`OutcomeSpace`](@ref).
+# A prominent example used in the NLTS literature are ordinal patterns.
 # The outcome space for it is [`SymbolicPermutation`](@ref), and can be particularly useful with timeseries that come from nonlinear dynamical systems. For example, if we simulate a logistic map timeseries
 
-using PredefinedDynamicalSystems
+using DynamicalSystemsBase
 
-ds = Systems.logistic(;r = 4)
+logistic_rule(u, r, t) = SVector(r*u[1]*(1 - u[1]))
+ds = DeterministicIteratedMap(logistic_rule, [0.4], 4.0)
 Y, t = trajectory(ds, 10_000; Ttr = 100)
-y = y[:, 1]
+y = Y[:, 1]
 summary(y)
 
 # we can estimate the probabilities corresponding to the ordinal patterns
@@ -110,5 +114,57 @@ probsy_bayes .- probsy
 
 # While the corrections of [`BayesianRegularization`](@ref) are small, they are nevertheless measurable. In truth, when calling [`probabilities`](@ref) with an outcome space instance, the default [`RelativeAmount`](@ref) probabilities estimator is used to extract the probabilities.
 
-# ## Entropies and information measures
+# ## Entropies
 
+# Many compexity measures are a straightforward estimation of Shannon entropy with a given outcome space.
+# For example, the well known _permutation entropy_ is exactly the Shannon entropy of the probabilities `probsy` we computed above based on ordinal patterns.
+
+perm_ent_x = entropy(SymbolicPermutation(), x)
+perm_ent_y = entropy(SymbolicPermutation(), y)
+(perm_ent_x, perm_ent_y)
+
+# As expected, the permutation entropy of the `x` signal is higher, because the signal is "more random".
+
+# Just like above with the possibility of many different outcome spaces, the same concept applies to entropy. There are many different entropies, Shannon is not the only one!
+# Each entropy is a subtype of [`EntropyDefinition`](@ref).
+# Another commonly used entropy is the Renyi or generalized entropy.
+# We can use [`Renyi`](@ref) as an additional first argument to the [`entropy`](@ref) function
+
+perm_ent_y_q2 = entropy(Renyi(;q = 2.0), SymbolicPermutation(), y)
+(perm_ent_y_q2, perm_ent_y)
+
+# In fact, when we called `entropy(SymbolicPermutation(), y)`, this dispatch to the default call of `entropy(Shannon(), SymbolicPermutation(), y)`.
+
+# ## More than entropies: estimators and other information measures
+
+# The estimation of an entropy truly parallelizes the estimation of probabilities: in the latter, we could decide an outcome space _and_ an _estimator_ to estimate probabilities.
+# The same happes for entropy: we can decide an entropy definition and an _estimator_ of how to estimate the entropy. For example, instead of the default [`PlugIn`](@ref) estimator that we used above implicitly, we could use [`Jackknife`](@ref)
+
+ospace = SymbolicPermutation()
+entdef = Renyi(;q = 2.0)
+entest = Jackknife(entdef)
+perm_ent_y_q2_jack = entropy(entest, ospace, y)
+
+(perm_ent_y_q2, perm_ent_y_q2_jack)
+
+# It is up to the researcher to read the documentation of the plethora of estimators implemented and decide what is most suitable for their data at hand. They all can be found in [`DiscreteInfoEstimator`](@ref).
+
+# Now, recall that at the very beginning of this notebook we mentioned a code separation of [`information`](@ref) and [`complexity`](@ref).
+# We did this because there are other measures, besides entropy, that are explicit functionals of some probability mass function.
+# One example is the Shannon _extropy_ [`ShannonExtropy`](@ref), the completent of _entropy_, which could be computed as
+
+extdef = ShannonExtropy()
+perm_ext_y = information(extdef, ospace, y)
+
+# and, just like the Shannon _entropy_, it could also be estimated with a different estimator such as [`Jackknife`](@ref)
+
+perm_ext_y_jack = information(Jackknife(extdef), ospace, y)
+
+# In truth, when we called `entropy(e, o, y)` it dispatched automatically to `information(e, o, y)`, as all "information measures" are part of the same function interface.
+
+# ## Beyond discrete: differential entropies
+
+
+# ## Beyond information: other complexity measures
+
+# As discussed at the very beginning of this tutorial, there are some complexity measures that are not explicit functionals of probabilities, and hence cannot be straightforwardly related to an outcome space, in the sense of providing an instance of [`OutcomeSpace`](@ref) to the estimation function.
