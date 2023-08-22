3255acff81e1543790205c231adac6bcc2e1d028
Kristian Haaga <kahaaga@gmail.com>
2023-08-22 13:21:04 +0200

    Test `CombinationEncoding`

---
+++ test/encodings/encodings/combination_encoding.jl
@@ -0,0 +1,14 @@
+x = rand(3) # xᵢ ∈ [0, 1] for all xᵢ in x
+e_ord = OrdinalPatternEncoding(length(x))
+e_amp = AmplitudeEncoding(0, 1, n = 3)
+e_firstdiff = FirstDifferenceEncoding(0, 1, n = 2)
+e_bin = RectangularBinEncoding(FixedRectangularBinning(0, 1, 2))
+es = [e_ord, e_firstdiff, e_amp, e_bin]
+
+e_combo = CombinationEncoding(es)
+
+symbol = encode(e_combo, x)
+@test symbol isa Int
+d = decode(e_combo, symbol)
+@test d isa AbstractVector
+@test length(d) == length(es)
