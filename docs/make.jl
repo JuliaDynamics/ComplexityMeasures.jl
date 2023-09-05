ce2e548aa15bfcc3de399ed0cd02a05239f1f237
Kristian Haaga <kahaaga@gmail.com>
2023-09-05 20:52:31 +0200

    Use main branch for DimensionalData for doc build

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 cd(@__DIR__)
 using ComplexityMeasures
+# Temporarily needed until new minor version of DimensionalData.jl is available
+# (current version is 0.24.14)
+using Pkg; Pkg.add(url="https://github.com/rafaqz/DimensionalData.jl#main")
 
 # Convert tutorial file to markdown
 import Literate
