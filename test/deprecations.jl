caaabb9d799efceecac5b6d43fda8f5b8f84f3ac
Datseris <datseris.george@gmail.com>
2023-11-08 13:27:32 +0000

    add deprecation test for Ordinal keuwrods

--- test/deprecations.jl
+++ test/deprecations.jl
@@ -1,27 +1,28 @@
 using ComplexityMeasures, Test
 
-x = randn(1000)
+@testset "2.0 deprecations" begin
+    x = randn(1000)
 
-# Convenience
-@test permentropy(x) == entropy_permutation(x; base=MathConstants.e)
-msg ="`permentropy(x; τ, m, base)` is deprecated.\nUse instead: `entropy_permutation(x; τ, m, base)`, or even better, use the\ndirect syntax discussed in the docstring of `entropy_permutation`.\n"
-@test_logs (:warn, msg) permentropy(x)
+    # Convenience
+    @test permentropy(x) == entropy_permutation(x; base=MathConstants.e)
+    msg ="`permentropy(x; τ, m, base)` is deprecated.\nUse instead: `entropy_permutation(x; τ, m, base)`, or even better, use the\ndirect syntax discussed in the docstring of `entropy_permutation`.\n"
+    @test_logs (:warn, msg) permentropy(x)
 
-# Generalized entropies
-@test genentropy(x, 0.1) == information(Shannon(MathConstants.e), ValueBinning(0.1), x)
-msg = "`genentropy(probs::Probabilities; q, base)` deprecated.\nUse instead: `information(Renyi(q, base), probs)`.\n"
-@test_logs (:warn, msg) genentropy(Probabilities(rand(3)))
+    # Generalized entropies
+    @test genentropy(x, 0.1) == information(Shannon(MathConstants.e), ValueBinning(0.1), x)
+    msg = "`genentropy(probs::Probabilities; q, base)` deprecated.\nUse instead: `information(Renyi(q, base), probs)`.\n"
+    @test_logs (:warn, msg) genentropy(Probabilities(rand(3)))
 
-msg = "`genentropy(x::Array_or_SSSet, est::ProbabilitiesEstimator; q, base)` is deprecated.\nUse instead: `information(Renyi(q, base), est, x)`.\n"
-@test_logs (:warn, msg) genentropy(x, ValueBinning(0.1))
+    msg = "`genentropy(x::Array_or_SSSet, est::ProbabilitiesEstimator; q, base)` is deprecated.\nUse instead: `information(Renyi(q, base), est, x)`.\n"
+    @test_logs (:warn, msg) genentropy(x, ValueBinning(0.1))
 
-@test probabilities(x, 0.1) == probabilities(ValueBinning(0.1), x)
-msg = "`probabilities(x, est::OutcomeSpace)`\nis deprecated, use `probabilities(est::OutcomeSpace, x) instead`.\n"
-@test_logs (:warn, msg) probabilities(x, ValueBinning(0.1))
+    @test probabilities(x, 0.1) == probabilities(ValueBinning(0.1), x)
+    msg = "`probabilities(x, est::OutcomeSpace)`\nis deprecated, use `probabilities(est::OutcomeSpace, x) instead`.\n"
+    @test_logs (:warn, msg) probabilities(x, ValueBinning(0.1))
 
-x = StateSpaceSet(rand(100, 3))
-@test genentropy(x, 4) == information(Shannon(MathConstants.e), ValueBinning(4), x)
-
+    x = StateSpaceSet(rand(100, 3))
+    @test genentropy(x, 4) == information(Shannon(MathConstants.e), ValueBinning(4), x)
+end
 
 @testset "deprecations: binning" begin
     @test FixedRectangularBinning((0, 1), (0, 1), 2) isa FixedRectangularBinning
@@ -31,7 +32,7 @@ end
 
 
 @testset "2.9 deprecations" begin
-    # For
+    x = StateSpaceSet(rand(100, 3))
 
     @test entropy_maximum(Shannon(MathConstants.e), ValueBinning(4), x) ==
         information_maximum(Shannon(MathConstants.e), ValueBinning(4), x)
@@ -42,4 +43,11 @@ end
     @test SymbolicPermutation() isa OrdinalPatterns
     @test SymbolicWeightedPermutation() isa WeightedOrdinalPatterns
     @test SymbolicAmplitudeAwarePermutation() isa AmplitudeAwareOrdinalPatterns
+
+    for f in (OrdinalPatterns, WeightedOrdinalPatterns, AmplitudeAwareOrdinalPatterns)
+        a = f(; m = 3, τ = 2)
+        b = f{3}(2)
+        @test entropy(a, x) == entropy(b, x)
+    end
+
 end
