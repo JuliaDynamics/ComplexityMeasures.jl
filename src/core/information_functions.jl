4fc1b0182e2cc3daebba3f494101e5ead8b7e79f
Datseris <datseris.george@gmail.com>
2023-12-14 14:31:00 +0000

    better info_max docstring

--- src/core/information_functions.jl
+++ src/core/information_functions.jl
@@ -143,15 +143,15 @@ end
 # Normalize API
 ###########################################################################################
 """
-    information_maximum(e::InformationMeasure, o::OutcomeSpace, x)
+    information_maximum(e::InformationMeasure, o::OutcomeSpace [, x])
 
 Return the maximum value of the given information measure can have, given input data `x`
-and  the given outcome space (the [`OutcomeSpace`](@ref) may also be specified by a
+and the given outcome space (the [`OutcomeSpace`](@ref) may also be specified by a
 [`ProbabilitiesEstimator`](@ref)).
 
 Like in [`outcome_space`](@ref), for some outcome spaces, the possible outcomes are known
 without knowledge of input `x`, in which case the function dispatches to
-`information_maximum(e, est)`.
+`information_maximum(e, o)`.
 
     information_maximum(e::InformationMeasure, L::Int)
 
