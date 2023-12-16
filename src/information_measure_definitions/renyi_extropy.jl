9726025f4137d65df229cdfda33e21605517ae8c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-12-16 13:39:20 +0100

    Fix crossref

--- src/information_measure_definitions/renyi_extropy.jl
+++ src/information_measure_definitions/renyi_extropy.jl
@@ -16,7 +16,7 @@ J_R(P) = \\dfrac{-(n - 1) \\log{(n - 1)} + (n - 1) \\log{ \\left( \\sum_{i=1}^N 
 
 for a probability distribution ``P = \\{p_1, p_2, \\ldots, p_N\\}``,
 with the ``\\log`` at the given `base`. Alternatively, `RenyiExtropy` can be used
-with [`extropy_normalized`](@ref), which ensures that the computed extropy is
+with [`information_normalized`](@ref), which ensures that the computed extropy is
 on the interval ``[0, 1]`` by normalizing to to the maximal Rényi extropy, given by
 
 ```math
