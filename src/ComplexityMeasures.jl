2c664de05d6ea8b1efd21315da04018a5a4c7378
George Datseris <datseris.george@gmail.com>
2024-01-09 13:55:08 +0000

    add latest news section to the docs (#364)
    * add latest news section to the docs
    
    * add discourse link

--- src/ComplexityMeasures.jl
+++ src/ComplexityMeasures.jl
@@ -39,41 +39,4 @@ include("complexity_measures/complexity_measures.jl")
 # deprecations (must be after all declarations)
 include("deprecations.jl")
 
-# Update messages:
-using Scratch
-display_update = false
-version_number = "2.0"
-update_name = "update_v$(version_number)"
-update_message = """
-Update message: ComplexityMeasures.jl v$(version_number)!
-
-Entropies.jl has been completely overhauled,
-and has been renamed to ComplexityMeasures.jl.
-
-Along with the overhaul comes a massive amount of new features, an entirely new API,
-extendable and educative code, dedicated documentation pages, and more!
-At the moment we estimate we offer at least 90 unique quantities characterizing
-complexity in the context of nonlinear dynamics and complex systems.
-We believe it is best to learn all of this by visiting the online documentation!
-
-We tried our best to keep pre-2.0 functions working and throw deprecation warnings.
-If we missed code that should be working, please let us know by opening an issue.
-
-For example, `geninformation(x::Array_or_SSSet, ε::Real; q, base)` is deprecated
-in favor of `information(Renyi(q, base), ValueBinning(ε), x)`.
-"""
-
-if display_update
-    # Get scratch space for this package
-    versions_dir = @get_scratch!("versions")
-    if !isfile(joinpath(versions_dir, update_name))
-        printstyled(
-            stdout,
-            update_message;
-            color = :light_magenta,
-        )
-        touch(joinpath(versions_dir, update_name))
-    end
-end
-
 end
