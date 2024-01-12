46e48eeae1c241994d2eddc7cba7be649174c476
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2024-01-12 16:36:41 +0100

    Deprecate ordinal outcome space constructors (#375)
    * Deprecate ordinal pattern outcome spaces
    
    * Deprecations
    
    * Parameterize on specific types
    
    * Test deprecations too
    
    * Remove shows and fix msg

--- test/deprecations.jl
+++ test/deprecations.jl
@@ -41,10 +41,102 @@ end
 
     @test entropy_normalized(Shannon(MathConstants.e), ValueBinning(4), x) ==
         information_normalized(Shannon(MathConstants.e), ValueBinning(4), x)
+    
+    @testset "Ordinal pattern constructiors" begin
+        @test SymbolicPermutation() isa OrdinalPatterns
+        @test SymbolicWeightedPermutation() isa WeightedOrdinalPatterns
+        @test SymbolicAmplitudeAwarePermutation() isa AmplitudeAwareOrdinalPatterns
 
-    @test SymbolicPermutation() isa OrdinalPatterns
-    @test SymbolicWeightedPermutation() isa WeightedOrdinalPatterns
-    @test SymbolicAmplitudeAwarePermutation() isa AmplitudeAwareOrdinalPatterns
+        @testset "OrdinalPatterns" begin 
+            msg = "Keyword argument `τ` to `OrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`OrdinalPatterns{m}(τ = 1, lt::Function = ComplexityMeasures.isless_rand)`" * 
+            ", so provide `τ` as a positional argument instead. " * 
+            "In this call, the given keyword `τ` is used instead of the positional `τ`."
+            τ = 1; 
+            @test_logs (:warn, msg) OrdinalPatterns{3}(τ + 1; τ)
+
+            msg = "Keyword argument `lt` to `OrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`OrdinalPatterns{m}(τ = 1, lt::Function = ComplexityMeasures.isless_rand)`" * 
+            ", so provide `lt` as a positional argument instead. "  * 
+            "In this call, the given keyword `lt` is used instead of the positional `lt`."
+            lt = Base.isless;
+            @test_logs (:warn, msg) OrdinalPatterns{3}(τ, lt; lt)
+
+            # Test that keyword argument is preferred over positional argument
+            o = OrdinalPatterns{3}(5; τ = 2)
+            @test o.τ == 2
+            lt = Base.isless;
+            ltr = ComplexityMeasures.isless_rand
+            o = OrdinalPatterns{3}(2, lt; lt = ltr)
+            @test o.encoding.lt == ltr
+        end
+
+        @testset "WeightedOrdinalPatterns" begin 
+            msg = "Keyword argument `τ` to `WeightedOrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`WeightedOrdinalPatterns{m}(τ::Int = 1, lt::F=ComplexityMeasures.isless_rand)`" * 
+            ", so provide `τ` as a positional argument instead. "  * 
+            "In this call, the given keyword `τ` is used instead of the positional `τ`."
+            τ = 1; 
+            @test_logs (:warn, msg) WeightedOrdinalPatterns{3}(τ + 1; τ)
+
+            msg = "Keyword argument `lt` to `WeightedOrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`WeightedOrdinalPatterns{m}(τ = 1, lt::Function = ComplexityMeasures.isless_rand)`" * 
+            ", so provide `lt` as a positional argument instead. "  * 
+            "In this call, the given keyword `lt` is used instead of the positional `lt`."
+            lt = Base.isless;
+            @test_logs (:warn, msg) WeightedOrdinalPatterns{3}(τ, lt; lt)
+
+            # Test that keyword argument is preferred over positional argument
+            o = WeightedOrdinalPatterns{3}(2; τ)
+            @test o.τ == WeightedOrdinalPatterns{3}(τ).τ
+            lt = Base.isless;
+            ltr = ComplexityMeasures.isless_rand
+            @test WeightedOrdinalPatterns{3}(2, lt; lt = ltr).encoding.lt == ltr
+        end
+
+        @testset "AmplitudeAwareOrdinalPatterns" begin 
+            msg = "Keyword argument `τ` to `AmplitudeAwareOrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`AmplitudeAwareOrdinalPatterns{m}(τ::Int = 1, A = 0.5, lt::F=isless_rand)`" * 
+            ", so provide `τ` as a positional argument instead. "  * 
+            "In this call, the given keyword `τ` is used instead of the positional `τ`."
+            τ = 1; 
+            @test_logs (:warn, msg) AmplitudeAwareOrdinalPatterns{3}(τ + 1; τ)
+
+            msg = "Keyword argument `lt` to `AmplitudeAwareOrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`AmplitudeAwareOrdinalPatterns{m}(τ::Int = 1, A = 0.5, lt::F=isless_rand)`" * 
+            ", so provide `lt` as a positional argument instead. "  * 
+            "In this call, the given keyword `lt` is used instead of the positional `lt`."
+            lt = Base.isless;
+            @test_logs (:warn, msg) AmplitudeAwareOrdinalPatterns{3}(τ, 0.5, lt; lt)
+            @test_throws ArgumentError AmplitudeAwareOrdinalPatterns{3}(τ, lt; lt)
+
+            msg = "Keyword argument `A` to `AmplitudeAwareOrdinalPatterns` is deprecated. " *
+            "The signature is now " * 
+            "`AmplitudeAwareOrdinalPatterns{m}(τ::Int = 1, A = 0.5, lt::F=isless_rand)`" * 
+            ", so provide `A` as a positional argument instead. "  * 
+            "In this call, the given keyword `A` is used instead of the positional `A`."
+            A = 0.5;
+            @test_logs (:warn, msg) AmplitudeAwareOrdinalPatterns{3}(τ, A; A)
+
+            # Test that keyword argument is preferred over positional argument
+            o = AmplitudeAwareOrdinalPatterns{3}(2; τ = 5)
+            @test o.τ == 5
+            lt = Base.isless;
+            ltr = ComplexityMeasures.isless_rand
+            o = AmplitudeAwareOrdinalPatterns{3}(2, 0.5, Base.isless; lt = ltr)
+            @test o.encoding.lt == ltr
+            o = AmplitudeAwareOrdinalPatterns{3}(2; τ = 5)
+            @test o.τ == 5
+            o = AmplitudeAwareOrdinalPatterns{3}(2, 0.5; A = 0.9)
+            @test o.A == 0.9
+        end
+    end
 
     for f in (OrdinalPatterns, WeightedOrdinalPatterns, AmplitudeAwareOrdinalPatterns)
         a = f(; m = 3, τ = 2)
