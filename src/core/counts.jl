dc0237b6f500209ebd730faf49196628152bfc17
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2024-01-14 13:53:30 +0100

    Better docstrings for probs and counts (#382)
    * Less brevity in exchange for more clarity.
    
    * Typo
    
    * Fewer lines
    
    * More nitpicking
    
    * Patch release
    
    * Update src/core/probabilities.jl
    
    Co-authored-by: George Datseris <datseris.george@gmail.com>
    
    ---------
    
    Co-authored-by: George Datseris <datseris.george@gmail.com>

--- src/core/counts.jl
+++ src/core/counts.jl
@@ -25,15 +25,15 @@ export is_counting_based
 ###########################################################################################
 """
     Counts <: Array{<:Integer, N}
-    Counts(counts [, outcomes [, outnames]]) → c
+    Counts(counts [, outcomes [, dimlabels]]) → c
 
 `Counts` stores an `N`-dimensional array of integer `counts` corresponding to a set of
 `outcomes`. This is typically called a "frequency table" or
 ["contingency table"](https://en.wikipedia.org/wiki/Contingency_table).
 
-If `c isa Counts`, then `c.outcomes[i]` is the outcomes along the `i`-th dimension,
-each being an abstract vector whose order is the same one corresponding to `c`,
-and `c.dimlabels[i]` is the label of the `i`-th dimension.
+If `c isa Counts`, then `c.outcomes[i]` is an abstract vector containing the outcomes
+along the `i`-th dimension, where `c[i][j]` is the count corresponding to the outcome
+`c.outcomes[i][j]`, and `c.dimlabels[i]` is the label of the `i`-th dimension.
 Both labels and outcomes are assigned automatically if not given.
 `c` itself can be manipulated and iterated over like its stored array.
 """
--- src/core/probabilities.jl
+++ src/core/probabilities.jl
@@ -10,17 +10,42 @@ export missing_outcomes
 ###########################################################################################
 """
     Probabilities <: Array{<:AbstractFloat, N}
-    Probabilities(counts/probs [, outcomes [, outnames]]) → p
+    Probabilities(probs::Array [, outcomes [, dimlabels]]) → p
+    Probabilities(counts::Counts [, outcomes [, dimlabels]]) → p
 
 `Probabilities` stores an `N`-dimensional array of probabilities, while ensuring that
-the array sums to 1 (normalized probability mass). The probabilities correspond to outcomes
-that describe the axes of the array. In most cases the array is a standard vector.
+the array sums to 1 (normalized probability mass). In most cases the array is a standard
+vector. `p` itself can be manipulated and iterated over, just like its stored array.
 
-If `p isa Probabilities`, then `p.outcomes[i]` is the outcomes along the `i`-th dimension,
-each being an abstract vector whose order is the same one corresponding to `p`,
-and `p.dimlabels[i]` is the label of the `i`-th dimension.
-Both labels and outcomes are assigned automatically if not given.
-`p` itself can be manipulated and iterated over like its stored array.
+
+The probabilities correspond to `outcomes` that describe the axes of the array. 
+If `p isa Probabilities`, then `p.outcomes[i]` is an an abstract vector containing 
+the outcomes along the `i`-th dimension. The outcomes have the same ordering as the 
+probabilities, so that `p[i][j]` is the probability for outcome `p.outcomes[i][j]`. 
+The dimensions of the array are named, and can be accessed by `p.dimlabels`, where 
+`p.dimlabels[i]` is the label of the `i`-th dimension. Both `outcomes` and `dimlabels`
+are assigned automatically if not given. If the input is a
+set of [`Counts`](@ref), and `outcomes` and `dimlabels` are not given, 
+then the labels and outcomes are inherited from the counts.
+
+## Examples 
+
+```julia
+julia> probs = [0.2, 0.2, 0.2, 0.2]; Probabilities(probs) # will be normalized to sum to 1
+ Probabilities{Float64,1} over 4 outcomes
+ Outcome(1)  0.25
+ Outcome(2)  0.25
+ Outcome(3)  0.25
+ Outcome(4)  0.25
+```
+
+```julia
+julia> c = Counts([12, 16, 12], ["out1", "out2", "out3"]); Probabilities(c)
+ Probabilities{Float64,1} over 3 outcomes
+ "out1"  0.3
+ "out2"  0.4
+ "out3"  0.3
+```
 """
 struct Probabilities{T, N, S} <: AbstractArray{T, N}
     # The probabilities table.
