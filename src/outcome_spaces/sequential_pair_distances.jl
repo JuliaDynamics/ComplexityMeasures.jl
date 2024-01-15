6c450ef922fb6db065b7c9b3d5873147ebec2b7f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-01-15 21:53:20 +0100

    Finish docstring

--- src/outcome_spaces/sequential_pair_distances.jl
+++ src/outcome_spaces/sequential_pair_distances.jl
@@ -4,10 +4,17 @@ export SequentialPairDistances
     SequentialPairDistances <: CountBasedOutcomeSpace
     SequentialPairDistances(x; n = 3, m = 3, τ = 1, metric = Distances.Chebyshev())
 
-The `SequentialPairDistances` outcome space. 
+The `SequentialPairDistances` outcome space, which is based on the distribution of 
+distances of sequential pairs of points.
 
-Input data `x` are needed initialization, because distances must be pre-computed to
-know the minimum/maximum distances needed for binning.
+This outcome space appears implicitly as part of the "distribution entropy" introduced
+by [Li2015](@citet), which of course can be reproduced here (see example below).
+We've generalized the method to be used with any [`InformationMeasure`](@ref) and
+[`DiscreteInfoEstimator`](@ref), and with valid distance `metric` (from Distances.jl).
+
+Input data `x` are needed for initialization, because distances must be pre-computed to
+know the minimum/maximum distances needed for binning the distribution of pairwise
+distances. 
 
 ## Description
 
@@ -17,11 +24,18 @@ know the minimum/maximum distances needed for binning.
     `m` and embedding lag `τ`.
 - Computes the distances `ds` between sequential pairs of points according to the given
     `metric`.
-- Divide the interval `[minimum(ds), maximum(ds)]` into `n` equal-size bins by using 
-    [`RectangularBinEncoding`](@ref). These bins are the outcomes, labeled as the 
-    integers `1:n`.
+- Divides the interval `[minimum(ds), maximum(ds)]` into `n` equal-size bins by using 
+    [`RectangularBinEncoding`](@ref), them maps the distances onto these bins.
 
-## Example
+## Outcome space
+
+The outcome space `Ω` for `SequentialPairDistances` are the bins onto which the 
+pairwise distances are mapped, encoded as the integers `1:n`. If you need the actual
+bin coordinates, these can be recovered with [`decode`](@ref) (see example below).
+
+## Examples
+
+The outcome bins can be retrieved as follows.
 
 ```julia
 using ComplexityMeasures
@@ -34,7 +48,16 @@ The outcome bins can be retrieved as follows.
 
 ```julia
 decode.(Ref(o.encoding.binencoder), outs)
-````
+```
+
+Computing the "distribution entropy" with `n = 3` bins for the distance histogram:
+
+```julia
+using ComplexityMeasures
+x = rand(1000000)
+o = SequentialPairDistances(x, n = 3, metric = Chebyshev()) # metric from original paper
+h = information(Shannon(base = 2), o, x)
+```
 """
 Base.@kwdef struct SequentialPairDistances{I<:Integer, M, T, DM, D, E} <: CountBasedOutcomeSpace
     n::I = 3
@@ -67,7 +90,6 @@ outcome_space(est::SequentialPairDistances) = collect(1:est.n)
 
 function counts_and_outcomes(o::SequentialPairDistances, x)
     binned_pts = encode.(Ref(o.encoding.binencoder), o.dists)
-    @show binned_pts
     return counts_and_outcomes(UniqueElements(), binned_pts)
 end
 
