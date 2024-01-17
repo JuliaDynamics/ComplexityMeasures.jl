550c444651053ee3fe1a09fbe511ece195783baf
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2024-01-17 02:10:55 +0100

    Update cross references and increment patch version (#392)

--- src/core/encodings.jl
+++ src/core/encodings.jl
@@ -16,6 +16,8 @@ Current available encodings are:
 - [`RelativeMeanEncoding`](@ref).
 - [`RelativeFirstDifferenceEncoding`](@ref).
 - [`UniqueElementsEncoding`](@ref).
+- [`BubbleSortSwapsEncoding`](@ref).
+- [`PairDistanceEncoding`](@ref).
 - [`CombinationEncoding`](@ref), which can combine any of the above encodings.
 """
 abstract type Encoding end
