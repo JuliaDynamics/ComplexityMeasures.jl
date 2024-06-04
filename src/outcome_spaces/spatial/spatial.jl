5478174a90d5d70be7c05ab47f014b510c077940
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2024-06-04 11:22:43 +0200

    Spatial bubble sort swaps (#393)
    * Spatial bubble sort swaps
    
    * Typo in changelog
    
    ---------
    
    Co-authored-by: George Datseris <datseris.george@gmail.com>

--- src/outcome_spaces/spatial/spatial.jl
+++ src/outcome_spaces/spatial/spatial.jl
@@ -4,3 +4,4 @@ abstract type SpatialOutcomeSpace{D, P} <: CountBasedOutcomeSpace end
 include("utils.jl")
 include("spatial_ordinal/SpatialOrdinalPatterns.jl")
 include("spatial_dispersion/SpatialDispersion.jl")
+include("spatial_bubble_sort_swaps/spatial_bubble_sort_swaps.jl")
