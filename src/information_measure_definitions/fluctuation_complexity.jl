805b60fbb896ed31628c9862d43214fd751cc0c5
Kristian Haaga (personal) <kahaaga@gmail.com>
2024-06-09 18:29:54 +0200

    Update `FluctuationComplexity` docstring

--- src/information_measure_definitions/fluctuation_complexity.jl
+++ src/information_measure_definitions/fluctuation_complexity.jl
@@ -2,7 +2,7 @@ export FluctuationComplexity
 
 """
     FluctuationComplexity <: InformationMeasure
-    FluctuationComplexity(; definition = Shannon(; base = 2), base = 2)
+    FluctuationComplexity(; definition = Shannon())
 
 The "fluctuation complexity" quantifies the standard deviation of the information content of the states 
 ``\\omega_i`` around some summary statistic ([`InformationMeasure`](@ref)) of a PMF. Specifically, given some 
@@ -10,19 +10,23 @@ outcome space ``\\Omega`` with outcomes ``\\omega_i \\in \\Omega``
 and a probability mass function ``p(\\Omega) = \\{ p(\\omega_i) \\}_{i=1}^N``, it is defined as
 
 ```math
-\\sigma_I(p) := \\sqrt{\\sum_{i=1}^N p_i(I_i - H_*)^2}
+\\sigma_I_Q(p) := \\sqrt{\\sum_{i=1}^N p_i(I_Q(p_i) - H_*)^2}
 ```
 
-where ``I_i = -\\log_{base}(p_i)`` is the information content of the i-th outcome. The type of information measure
-``*`` is controlled by `definition`. 
+where ``I_Q(p_i)`` is the [`self_information`](@ref) of the i-th outcome with respect to the information 
+measure of type ``Q`` (controlled by `definition`).
 
-The `base` controls the base of the logarithm that goes into the information content terms. Make sure that 
-you pick a `base` that is consistent with the base chosen for the `definition` (relevant for e.g. [`Shannon`](@ref)).
+## Compatible with
+
+- [`Shannon`](@ref)
+- [`Tsallis`](@ref)
+- [`Curado`](@ref)
+- [`ShannonExtropy`](@ref)
 
 ## Properties 
 
-If `definition` is the [`Shannon`](@ref) entropy, then we recover 
-the [Shannon-type information fluctuation complexity](https://en.wikipedia.org/wiki/Information_fluctuation_complexity) 
+If `definition` is the [`Shannon`](@ref) entropy, then we recover the 
+[Shannon-type information fluctuation complexity](https://en.wikipedia.org/wiki/Information_fluctuation_complexity) 
 from [Bates1993](@cite). Then the fluctuation complexity is zero for PMFs with only a single non-zero element, or 
 for the uniform distribution.
 
@@ -32,7 +36,8 @@ properties [Bates1993](@cite).
 !!! note "Potential for new research" 
     As far as we know, using other information measures besides Shannon entropy for the 
     fluctuation complexity hasn't been explored in the literature yet. Our implementation, however, allows for it.
-    Please inform us if you try some new combinations!
+    We're currently writing a paper outlining the generalizations to other measures. For now, we verify 
+    correctness of the measure through numerical examples in out test-suite.
 """
 struct FluctuationComplexity{M <: InformationMeasure, I <: Integer} <: InformationMeasure
     definition::M
