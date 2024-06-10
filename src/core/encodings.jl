dd1c61daefc3f2582f7923e3d39c7494e01f91c9
George Datseris <datseris.george@gmail.com>
2024-06-10 10:47:34 +0100

    Update `encode` docstring
    had incorrect greater equal 0. can never be zero.

--- src/core/encodings.jl
+++ src/core/encodings.jl
@@ -25,8 +25,8 @@ abstract type Encoding end
 """
     encode(c::Encoding, χ) -> i::Int
 
-Encode an element `χ ∈ x` of input data `x` (those given to [`counts`](@ref))
-into the positive integers `i ≥ 0` using encoding `c`.
+Encode an element `χ ∈ x` of input data `x` (those given to e.g., [`counts`](@ref))
+into the **positive integers** using encoding `c`.
 The special value of `i = -1` is used as a return value for
 inappropriate elements `χ` that cannot be encoded according to `c`.
 """
