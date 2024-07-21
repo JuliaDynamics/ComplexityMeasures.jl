cc3975d3578d5cb7881fcef24cdfc050090276c0
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-21 09:47:00 +0200

    Add tests

--- test/encodings/encodings/unique_elements_encoding.jl
+++ test/encodings/encodings/unique_elements_encoding.jl
@@ -1,6 +1,11 @@
 using Test
 using ComplexityMeasures
+using statespacesets
 
 x = ['a', 2, 5, 2, 5, 'a']
 e = UniqueElementsEncoding(x)
 @test encode.(Ref(e), x) == [1, 2, 3, 2, 3, 1]
+
+y = StateSpaceSet(["a", "b", "c", "b", "a"])
+ey = UniqueElementsEncoding(y)
+encode.(Ref(ey), y)
