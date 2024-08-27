02c3153242a2e5a75b16b5165b977ec9c69d2edc
rusandris <rusandris@gmail.com>
2024-08-27 17:42:20 +0300

    add logistic map test

--- test/outcome_spaces/implementations/transfer_operator.jl
+++ test/outcome_spaces/implementations/transfer_operator.jl
@@ -1,5 +1,8 @@
+using Revise 
 using ComplexityMeasures, Test
 using Random: MersenneTwister
+using DynamicalSystemsBase
+using Integrals
 @test TransferOperator(RectangularBinning(3)) isa TransferOperator
 
 D = StateSpaceSet(rand(MersenneTwister(1234), 100, 2))
@@ -18,8 +21,47 @@ binnings = [
 # probabilities, because they are computed according to the approximation to the
 # transfer operator, not as naive visitation freqiencies to the bins. We just test
 # generically here.
+
 # TODO: make a stupidly simple example where we can actually compute the measure of
 # each bin exactly.
+
+#logistic map r=4 -> invariant measure is known analytically ρ(x) = 1/(π√x(1-x))
+
+@testset begin "Analytical logistic test probabilities" 
+
+    ρ(x,p) = 1/(π*sqrt(x*(1-x))) #analytic invariant measure
+
+    #-----------------logistic map orbit-------------------
+    logistic_rule(x, p, n) = SVector{1}(p[1]*x[1]*(1.0 - x[1]))
+    r = 4.0; p = [r]
+    ds = DeterministicIteratedMap(logistic_rule,[0.4],p)
+    orbit,t = trajectory(ds, 10^7; Ttr = 10^4)
+
+    #--------------estimate invariant measure----------
+    b = RectangularBinning(10, true)
+    to = ComplexityMeasures.transferoperator(orbit, b)
+    iv = invariantmeasure(to)
+    p,outcomes = invariantmeasure(iv)
+
+    #order from leftmost bin to rightmost bin  
+    p_bins = p[sortperm(outcomes)]
+
+    #----------compute probability for each bin analytically----------
+    bin_ranges = to.encoding.ranges[1]
+    ρ_bins = zeros(10)
+
+    for i in 1:length(bin_ranges)-1
+        domain = (bin_ranges[i], bin_ranges[i+1]) # (lb, ub)
+        prob = IntegralProblem(ρ, domain, p)
+        sol = solve(prob, QuadGKJL())
+        ρ_bins[i] = sol[1]
+    end
+
+    #--------------test if they're equal---------------
+    @show p_bins,ρ_bins
+    @test all(isapprox.(p_bins,ρ_bins;atol=1e-3))
+
+end
 
 @testset "Binning test $i" for i in eachindex(binnings)
     b = binnings[i]
