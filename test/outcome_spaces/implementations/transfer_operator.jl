249641e3c03384641bcc0c0f11a1c4866987059e
rusandris12 <rusandris@gmail.com>
2026-01-06 21:58:43 +0200

    test transferoperator api

--- test/outcome_spaces/implementations/transfer_operator.jl
+++ test/outcome_spaces/implementations/transfer_operator.jl
@@ -2,6 +2,7 @@ using ComplexityMeasures, Test
 using Random: MersenneTwister
 using DynamicalSystemsBase
 using Integrals
+cd(@__DIR__)
 
 #test for all count-based outcome spaces
 
@@ -35,29 +36,12 @@ using Integrals
 
 end
 
-@test TransferOperator(RectangularBinning(3)) isa TransferOperator
-
-D = StateSpaceSet(rand(MersenneTwister(1234), 100, 2))
-
-# Note that if `false` is used for `precise` the tests will fail.
-# But that's okay, since we do not do guarantees for that case.
-binnings = [
-    RectangularBinning(3, true),
-    RectangularBinning(0.2, true),
-    RectangularBinning([2, 3], true),
-    RectangularBinning([0.2, 0.3], true),
-    FixedRectangularBinning(range(0, 1; length = 5), 2, true)
-]
-
-# There's not easy way of constructing an analytical example for the resulting
-# probabilities, because they are computed according to the approximation to the
-# transfer operator, not as naive visitation freqiencies to the bins. We just test
-# generically here.
-
-# TODO: make a stupidly simple example where we can actually compute the measure of
-# each bin exactly.
-
 #logistic map r=4 -> invariant measure is known analytically ρ(x) = 1/(π√x(1-x))
+
+#stupidly simple example where we can actually compute the measure of
+#each bin exactly. Calculate numerically integrated probabilities of bins for the analytical 
+#invariant measure of the logistic map. Compare with probabilities, computed according to the 
+#approximation to the transfer operator, not as naive visitation freqiencies to the bins.
 
 @testset begin "Analytical logistic test probabilities" 
 
@@ -96,9 +80,24 @@ binnings = [
 
 end
 
+#test every kind of RectangularBinning with transferoperator
+
+D = StateSpaceSet(rand(MersenneTwister(1234), 100, 2))
+
+# Note that if `false` is used for `precise` the tests will fail.
+# But that's okay, since we do not do guarantees for that case.
+binnings = [
+    RectangularBinning(3, true),
+    RectangularBinning(0.2, true),
+    RectangularBinning([2, 3], true),
+    RectangularBinning([0.2, 0.3], true),
+    FixedRectangularBinning(range(0, 1; length=5), 2, true)
+]
+
 @testset "Binning test $i" for i in eachindex(binnings)
+    @show i
     b = binnings[i]
-    to = ComplexityMeasures.transferoperator(ValueBinning(b),D)
+    to = transferoperator(ValueBinning(b),D)
     @test to isa ComplexityMeasures.TransferOperatorApproximation
 
     iv = invariantmeasure(to)
@@ -118,8 +117,9 @@ end
 # Warn if we're not using precise binnings.
 imprecise_warning = "`binning.precise == false`. You may be getting points outside the binning."
 b = RectangularBinning(5)
-@test_logs (:warn, imprecise_warning) ComplexityMeasures.transferoperator(D, b; warn_precise = true)
+@test_logs (:warn, imprecise_warning) transferoperator(ValueBinning(b), D; warn_precise = true)
 
+#=
 # ---------------
 # Reproducibility
 # ---------------
@@ -135,3 +135,50 @@ p1 = probabilities(TransferOperator(b; rng), D)
 p2 = probabilities(TransferOperator(b; rng), D)
 @test !all(p1 .== p2)
 @test p1[1] ≈ p2[1] # But we should be close
+=#
+
+
+#------------test API-------------
+
+@testset begin "Test high-level invariantmeasure method"
+    x = rand(1000)
+    o = OrdinalPatterns{3}()
+
+    #iterative method
+    im_iter = invariantmeasure(o, x; method=:iterate)
+    @test im_iter.to isa ComplexityMeasures.TransferOperatorApproximation
+    ρ_iter = im_iter.ρ
+    @test ρ_iter isa Probabilities
+
+    #eigenvec method
+    im_eigen = invariantmeasure(o, x; method=:eigen)
+    @test im_eigen.to isa ComplexityMeasures.TransferOperatorApproximation
+    ρ_eigen = im_eigen.ρ
+    @test ρ_eigen isa Probabilities
+
+    #compare if they're equal
+    @test all(isapprox.(ρ_iter,ρ_eigen))
+
+    #test through probabilities interface and compare 
+
+    #try logistic map
+    logistic_rule(x, p, n) = SVector{1}(p[1] * x[1] * (1.0 - x[1]))
+    r = 4.0
+    p = [r]
+    ds = DeterministicIteratedMap(logistic_rule, [0.4], p)
+    orbit, t = trajectory(ds, 10^7; Ttr=10^4)
+
+    #try binning on logistic
+    os = ValueBinning(RectangularBinning(10, true))
+    p_TO = probabilities(TransferOperator(),os,orbit) #correct but and ordered as RelativeAmount
+    p = probabilities(RelativeAmount(), os, orbit)
+    @test all(isapprox.(p_TO.p, p.p; atol=1e-3))
+
+    #try op on logistic
+    op = OrdinalPatterns{3}()
+    p_TO = probabilities(TransferOperator(), op, orbit) #correct but not ordered as RelativeAmount
+    p = probabilities(RelativeAmount(), op, orbit)
+    @show p_TO.p 
+    @show p.p
+    @test !all(isapprox.(p_TO.p, p.p; atol=1e-3))
+end
